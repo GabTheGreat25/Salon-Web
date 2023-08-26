c852dc96d1ab9042a6513c9d97c4a5b907143e49
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-26 20:11:02 +0800

    Add NotFound Image

--- src/layouts/NotFound.jsx
+++ src/layouts/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import NotFound from "@assets/404-Not-Found.png";
 
 export default function () {
   const goBack = () => {
@@ -7,11 +8,14 @@ export default function () {
 
   return (
     <>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
       <div>
         <h1>Not Found</h1>
         <h2>The requested page could not be found.</h2>
         <br />
-        <button title="Go Back" onClick={goBack}></button>
+        <button title="Go Back" onClick={goBack}>
+          Go Back
+        </button>
       </div>
     </>
   );
