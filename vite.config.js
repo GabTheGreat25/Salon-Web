83e8161633cdce1d46f17ae3076c0258383832d3
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-26 20:08:26 +0800

    Add million js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "url";
+import million from "million/compiler";
 
 const srcPath = fileURLToPath(new URL("./src", import.meta.url));
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), million.vite({ auto: true })],
   resolve: {
     alias: {
       "@": srcPath,
-      // "@assets": `${srcPath}/assets`,
+      "@assets": `${srcPath}/assets`,
       // "@generateKey": `${srcPath}/services/generateKey`,
       // "@dataTable": `${srcPath}/services/dataTable`,
       // "@component": `${srcPath}/component`,
