9b0f9dfa2ad2c993a05d465d9e0b8bec87326e71
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-27 23:42:37 +0800

    Update facebook id

--- src/components/Facebook-Messenger/index.jsx
+++ src/components/Facebook-Messenger/index.jsx
@@ -5,7 +5,7 @@ import { FacebookProvider, CustomChat } from "react-facebook";
 export default function () {
   return (
     <>
-      <FacebookProvider appId="1000144684738483" chatSupport>
+      <FacebookProvider appId="625328572915568" chatSupport>
         <CustomChat pageId="127317223684561" minimized="true" />
       </FacebookProvider>
     </>
