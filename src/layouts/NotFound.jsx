0900a9960a2a7ced479f8206ea27378eb4b1e4b0
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-27 20:40:12 +0800

    Add dark mode logic with toggle

--- src/layouts/NotFound.jsx
+++ src/layouts/NotFound.jsx
@@ -8,15 +8,19 @@ export default function () {
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen">
-        <img src={NotFound} alt="NotFound" className="w-[34rem]" />
+      <div className="flex flex-col items-center justify-center h-[90vh]">
+        <img
+          src={NotFound}
+          alt="NotFound"
+          className="md:w-[26rem] lg:w-[30rem]"
+        />
         <h1 className="text-4xl font-semibold">Not Found</h1>
         <h2 className="text-xl">The requested page could not be found.</h2>
         <br />
         <button
           title="Go Back"
           onClick={goBack}
-          className="px-12 py-4 text-xl font-medium hover:text-white hover:animate-bounce animate-none highlight highlight-variant-12 after:bg-gradient-to-tr after:from-secondary-default after:to-primary-default"
+          className="px-12 py-4 text-xl font-medium transition-transform duration-300 ease-in-out hover:text-light-default hover:dark:text-dark-default text hover:animate-bounce animate-none highlight highlight-variant-12 after:bg-gradient-to-tr after:from-secondary-default after:to-primary-default"
         >
           Go Back
         </button>
