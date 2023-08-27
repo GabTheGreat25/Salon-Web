31fca9dc5578c717fe5462f9cd58204514dc5ad6
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-27 20:40:01 +0800

    Add DashboardLayout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,7 @@
 import RootLayout from "./RootLayout";
 import NotFound from "./NotFound";
 import Welcome from "./Welcome";
+import DashboardLayout from "./DashboardLayout";
+import PublicLayout from "./PublicLayout";
 
-export { RootLayout, NotFound, Welcome };
+export { RootLayout, NotFound, Welcome, DashboardLayout, PublicLayout };
