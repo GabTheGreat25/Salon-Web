2c4c764fcd256c4db44f7b9aaf66861ddf34e686
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-27 14:10:36 +0800

    Add useful tailwind plugins

--- src/layouts/NotFound.jsx
+++ src/layouts/NotFound.jsx
@@ -1,21 +1,130 @@
-import React from "react";
+import React, { useState } from "react";
 import NotFound from "@assets/404-Not-Found.png";
 
 export default function () {
+  const [clamped, setClamped] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+
   const goBack = () => {
     window.history.back();
   };
 
+  const toggleClamp = () => {
+    setClamped(!clamped);
+  };
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-      <div>
-        <h1>Not Found</h1>
-        <h2>The requested page could not be found.</h2>
-        <br />
-        <button title="Go Back" onClick={goBack}>
-          Go Back
-        </button>
+      <div className="container prose dark debug-screens peer/name hover:text-red-500">
+        <div class="perspective-9">
+          <div class="w-40 h-40 p-4 bg-red-500 translate-z-20 rotate-x-30 rotate-y-35 -rotate-z-20">
+            <h2>3D transform</h2>
+          </div>
+        </div>
+        <div
+          class="
+              inline-block
+              px-4
+              py-2
+              font-bold
+              text-center text-gray-900
+              transition-all
+              bg-white
+              border-2 border-black
+              rounded-md
+              active:-translate-z-10
+              hover:bg-gray-900
+              hover:text-white
+              hover:-translate-z-5
+              hover:-rotate-z-12
+              hover:rotate-y-20
+              hover:rotate-x-20
+              hover:shadow-2xl
+            "
+        >
+          Cube
+        </div>
+        <div class="p-20">
+          <div class="perspective-9">
+            <div class="w-40 h-40 p-4 bg-green-500 transform-style-3d rotate-x-30 rotate-y-35 -rotate-z-20">
+              <div class="flex justify-end">
+                <p class="p-2 translate-x-10 -translate-y-6 border border-black shadow-xl bg-white/70 translate-z-16 -rotate-x-10 -rotate-y-30 rotate-z-20">
+                  3D transform
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <h1 className="underline">Hello world!</h1>
+        <input className="autofill:border-gray-900 autofill:shadow-fill-white autofill:text-fill-gray-900" />
+        <p className={clamped ? "line-clamp-2" : ""}>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
+          quidem sint voluptas et sunt? Provident eaque, soluta maxime aliquam
+          perferendis eveniet necessitatibus ullam harum nobis alias fugiat
+          mollitia reprehenderit aspernatur repellat ex quia autem? Vel,
+          dolorem. Nostrum fugit in harum at magnam ducimus aperiam fugiat vero
+          ad. Veniam, quos repudiandae! Aut est tempora dolore saepe in ullam
+          voluptatum non sapiente similique earum explicabo eligendi quasi
+          aspernatur, rem corporis quaerat velit illum id neque maiores
+          corrupti? Recusandae repellat, veritatis repudiandae omnis architecto
+          quae nihil. Laboriosam, suscipit officia incidunt earum quis laborum
+          labore, consequatur dolorum voluptatibus facere dolorem, corporis
+          reiciendis facilis error?
+        </p>
+        <div class="filter filter-blur-2 filter-hue-rotate-90 sm:filter-blur-8">
+          Some awesome filters
+        </div>
+        <div className={darkMode ? "dark:bg-dark-default" : ""}>
+          <h1 className={darkMode ? "dark:text-white" : ""}>Not Found</h1>
+          <h2>The requested page could not be found.</h2>
+          <button
+            title="Toggle Dark Mode"
+            onClick={toggleDarkMode}
+            className="btn btn-base-100"
+          >
+            {!darkMode ? "Yes" : "No"}
+          </button>
+          <br />
+          <button
+            title="Toggle Clamp"
+            onClick={toggleClamp}
+            className="btn btn-base-100"
+          >
+            {clamped ? "Show More" : "Show Less"}
+          </button>
+          <button
+            title="Go Back"
+            onClick={goBack}
+            className="highlight highlight-indigo-600 highlight-variant-7 highlight-spread-md text-primary-default"
+          >
+            Go Back
+          </button>
+        </div>
+      </div>
+      <div class="group-custom">
+        <div class="group-1">
+          <div class="group-1-active:text-slate-500">Test</div>
+        </div>
+        <div class="group-custom-active:bg-pink-200">Again</div>
+      </div>
+      <br />
+      <br />
+      <div class="overflow-x-auto whitespace-nowrap w-24 scrollbar-none">
+        Some long text which will overflow for sure. Please is it enough?
+        Surely.
+      </div>
+      <div className=" peer-hover/name:text-red-500 animate-bounce">test</div>
+      <div class="aspect-w-16 aspect-h-9">
+        <iframe
+          src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
       </div>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: true,
+  prefix: "",
+  darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  purge: {
+    enabled: true,
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  },
+  mode: "jit",
+  corePlugins: {
+    aspectRatio: false,
+    preflight: true,
+  },
   theme: {
     screens: {
       xxs: "320px",
@@ -11,11 +23,59 @@ module.exports = {
       xl: "1280px",
       "2xl": "1536px",
     },
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
     extend: {
+      borderColor: ["autofill"],
+      shadowFill: ["autofill"],
+      textFill: ["autofill"],
+      scrollbar: {
+        DEFAULT: {
+          size: "spacing.1",
+          track: { background: "lightgray" },
+          thumb: { background: "gray" },
+          hover: { background: "darkgray" },
+        },
+        thin: {
+          size: "2px",
+          track: { background: "lightgray" },
+          thumb: { background: "gray" },
+          hover: { background: "darkgray" },
+        },
+        primary: {
+          size: "1rem",
+          track: { background: "#FED3EF" },
+          thumb: { background: "#FDA7DF" },
+          hover: { background: "#F78FB3" },
+        },
+        secondary: {
+          size: ".5rem",
+          track: { background: "#FED3EF" },
+          thumb: { background: "#FDA7DF" },
+          hover: { background: "#F78FB3" },
+        },
+      },
+      tooltipArrows: (theme) => ({
+        "danger-arrow": {
+          borderColor: theme("colors.red.400"),
+          borderWidth: 1,
+          backgroundColor: theme("colors.red.200"),
+          size: 10,
+          offset: 10,
+        },
+      }),
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
       colors: {
+        light: {
+          default: "#e5e5e5",
+        },
+        dark: {
+          default: "#212B36",
+        },
         primary: {
           default: "#FDA7DF",
           variant: "#FDB9E5",
@@ -38,7 +98,6 @@ module.exports = {
           800: "#333F4D",
           700: "#425263",
           600: "#516579",
-          500: "#5E738A",
           300: "#8D9DAE",
           200: "#ADB9C6",
           100: "#CCD5DE",
@@ -47,5 +106,45 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: false,
+    base: true,
+    styled: true,
+    utils: true,
+    rtl: false,
+    prefix: "",
+    logs: true,
+    themes: [
+      {
+        mytheme: {
+          primary: "#FDA7DF",
+          secondary: "#E056FD",
+          accent: "#F78FB3",
+          neutral: "#BE2EDD",
+          "base-100": "#CCD5DE",
+          info: "#248EF2",
+          success: "#39B54A",
+          warning: "#FFB400",
+          error: "#FF0000",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@kamona/tailwindcss-perspective"),
+    require("@gradin/tailwindcss-scrollbar"),
+    require("tailwindcss-labeled-groups")(["custom", "1"]),
+    require("tailwindcss-css-filters"),
+    require("tailwindcss-highlights"),
+    require("tailwindcss-autofill"),
+    require("tailwindcss-tooltip-arrow-after"),
+    require("tailwindcss-debug-screens"),
+    require("tailwindcss-gradients"),
+    require("tailwindcss-border-gradients")(),
+    require("daisyui"),
+  ],
 };
