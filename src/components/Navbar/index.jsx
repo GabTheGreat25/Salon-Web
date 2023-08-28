1ea979ae2763e3411862abd298a3a32e80852b9d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-28 15:45:12 +0800

    Add footer icons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="navbar ">
+      <div className="navbar">
         <div className="flex-1">
           <a className="text-xl normal-case btn btn-ghost hover:dark:bg-light-default hover:dark:text-dark-default">
             Lhanlee Salon
