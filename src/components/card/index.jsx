bdba9e0211ba95df96938dbe1eb9fe695e9262dc
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-30 00:02:42 +0800

    Made to BEM methodology and remove million js

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,22 +1,17 @@
 import React from "react";
-import { block } from "million/react";
 import LogoLight from "@assets/logo-Light.png";
 
-const Card = () => {
+export default function ({ children }) {
   return (
     <>
-      <div className="grid h-full grid-cols-2 bg-primary-variant">
-        <div className="grid items-center justify-end object-contain w-auto h-auto max-w-full max-h-full ml-3">
+      <div class="customCard">
+        <div class="customCard__logo">
           <img src={LogoLight} alt="LogoLight" />
         </div>
         <div>
-          <div className="h-full p-4 bg-white rounded-bl-[2.75rem] shadow-lg rounded-tl-[2.75rem]"></div>
+          <div class="customCard__children">{children}</div>
         </div>
       </div>
     </>
   );
-};
-
-const CardBlock = block(Card);
-
-export default CardBlock;
+}
