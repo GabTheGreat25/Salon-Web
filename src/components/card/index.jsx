9edfd4ccb87387ca525f7c0d187e44a546d07f72
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-30 15:16:14 +0800

    Add go back arrow globally

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import LogoLight from "@assets/Logo-Light.png";
+import { FaArrowLeft } from "react-icons/fa";
 
 export default function ({ children }) {
+  const goBack = () => {
+    window.history.back();
+  };
   return (
     <>
-      <div class="customCard">
-        <div class="customCard__logo">
+      <div className="customCard">
+        <div className="customCard__logo">
           <img src={LogoLight} alt="LogoLight" />
         </div>
         <div>
-          <div class="customCard__children">{children}</div>
+          <div className="customCard__children">
+            <button
+              className="customCard__children--arrowLeft"
+              onClick={goBack}
+            >
+              <FaArrowLeft />
+            </button>
+            {children}
+          </div>
         </div>
       </div>
     </>
