58ec29f7aa7a567f752cfc35089d35ea4946fa45
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-30 15:15:23 +0800

    Add EmployeeSignUp

---
+++ src/pages/EmployeeSignUp/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Card } from "@components";
+
+export default function () {
+  return (
+    <>
+      <Card>
+        <div className="w-full h-full text-light-default dark:text-dark-default"></div>
+      </Card>
+    </>
+  );
+}
