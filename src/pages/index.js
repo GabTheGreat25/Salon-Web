323ec553d07ecc743777e3a4cdc9dc4c02e438cc
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-30 15:15:41 +0800

    Add CustomerSignUp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,6 @@
 import ForgotPassword from "./ForgotPassword";
 import ChooseRole from "./ChooseRole";
+import CustomerSignUp from "./CustomerSignUp";
+import EmployeeSignUp from "./EmployeeSignUp";
 
-export { ForgotPassword, ChooseRole };
+export { ForgotPassword, ChooseRole, CustomerSignUp, EmployeeSignUp };
