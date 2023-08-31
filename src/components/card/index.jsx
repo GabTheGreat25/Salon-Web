99f78df1ff1d2656b25bfd7c23bd57f070ba35ba
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-31 14:40:20 +0800

    Add million js to card

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,29 +1,28 @@
 import React from "react";
+import { block } from "million/react";
 import LogoLight from "@assets/Logo-Light.png";
 import { FaArrowLeft } from "react-icons/fa";
 
-export default function ({ children }) {
+const Card = ({ children }) => {
   const goBack = () => {
     window.history.back();
   };
+
   return (
-    <>
-      <div className="customCard">
-        <div className="customCard__logo">
-          <img src={LogoLight} alt="LogoLight" />
-        </div>
-        <div>
-          <div className="customCard__children">
-            <button
-              className="customCard__children--arrowLeft"
-              onClick={goBack}
-            >
-              <FaArrowLeft />
-            </button>
-            {children}
-          </div>
-        </div>
+    <div className="customCard">
+      <div className="customCard__logo">
+        <img src={LogoLight} alt="LogoLight" />
       </div>
-    </>
+      <div className="customCard__children">
+        <button className="customCard__children--arrowLeft" onClick={goBack}>
+          <FaArrowLeft />
+        </button>
+        {children}
+      </div>
+    </div>
   );
-}
+};
+
+const CardBlock = block(Card);
+
+export default CardBlock;
