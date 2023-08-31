ba1374763a1c9b70e6fd1119c1b79f5c0dd9d209
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-08-31 13:27:53 +0800

    Change first letter to capital

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import FacebookMessenger from "./FacebookMessenger";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Loader from "./Loader";
-import Card from "./card";
+import Card from "./Card";
+import SignUp from "./SignUp";
 
-export { FacebookMessenger, Navbar, Footer, Loader, Card };
+export { FacebookMessenger, Navbar, Footer, Loader, Card, SignUp };
