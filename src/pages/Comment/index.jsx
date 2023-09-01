4ccd7fa4aace10543a8bbc43d73cb11b50cf5534
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-02 01:53:48 +0800

    Fix ui of comment

--- src/pages/Comment/index.jsx
+++ src/pages/Comment/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { CardImage } from "@components";
-import { FaArrowLeft } from "react-icons/fa";
+import { Card, CardImage } from "@components";
 import CameraImg from "@assets/Camera.png";
 import VideoImg from "@assets/Video.png";
 import StarImg from "@assets/Star.png";
@@ -12,69 +11,79 @@ export default function () {
     navigate("/");
   };
 
-  const goBack = () => {
-    window.history.back();
-  };
-
   return (
     <>
-      <div className="grid h-full ">
-        <div className="grid grid-cols-2 gap-20">
-          <div className="relative">
-            <button
-              onClick={goBack}
-              className="absolute text-3xl transform -translate-y-1/2 left-4 top-10"
-            >
-              <FaArrowLeft />
-            </button>
+      <Card>
+        <div className="grid w-full h-full text-light-default dark:text-dark-default">
+          <span className="grid items-end justify-center">
+            <h1 className="font-semibold lg:text-5xl md:text-4xl">
+              Leave us a comment!
+            </h1>
+          </span>
+          <div className="grid grid-cols-[40%_60%] items-center justify-start pt-6 gap-x-6">
             <span className="grid items-end justify-end h-[90%] mt-20">
               <CardImage />
             </span>
-          </div>
-          <div className="pr-10">
-            <div className="grid grid-flow-col pt-48">
-              <h1 className="lg:text-3xl md:text-xl text-neutral-secondary">
-                Leave us a comment
+            <div className="grid justify-center grid-flow-row-dense pr-10 gap-y-4">
+              <label className="block">
+                <span className="font-semibold xl:text-xl lg:text-[.8rem] md:text-[.55rem]">
+                  <p className="text-center">
+                    How was your experience with our services?
+                  </p>
+                </span>
+                <textarea
+                  className="resize-none block mb-10 mt-4 xl:text-xl lg:text-[1rem] md:text-sm placeholder-white border-2 bg-card-input w-full border-light-default dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default"
+                  rows="6"
+                ></textarea>
+                <h5 className="text-center xl:text-2xl lg:text-base md:text-sm">
+                  You can also
+                </h5>
+                <div className="grid grid-cols-[50%_auto_50%]">
+                  <div className="grid grid-rows-[80%_5%] items-center justify-center">
+                    <span className="grid items-center justify-center">
+                      <img
+                        src={CameraImg}
+                        alt="CameraImg"
+                        className="w-full h-1/2"
+                      />
+                    </span>
+                    <button className="text-center xl:text-4xl lg:text-2xl md:text-lg hover: hover:text-primary-default">
+                      Add Photo
+                    </button>
+                  </div>
+                  <span className="grid items-center justify-center">
+                    <h5 className="lg:text-xl md:text-lg">or</h5>
+                  </span>
+                  <div className="grid grid-rows-[80%_5%] items-center justify-center">
+                    <span className="grid items-center justify-center">
+                      <img
+                        src={VideoImg}
+                        alt="VideoImg"
+                        className="w-full h-1/2"
+                      />
+                    </span>
+                    <button className="text-center xl:text-4xl lg:text-2xl md:text-lg hover:text-primary-default">
+                      Add Video
+                    </button>
+                  </div>
+                </div>
+              </label>
+              <hr />
+              <span className="grid items-start justify-center pt-6 cursor-pointer">
+                <img src={StarImg} alt="StarImg" />
+              </span>
+              <h1 className="pt-6 text-center xl:text-3xl lg:text-2xl md:text-lg">
+                Show us some love and give us some rate!
               </h1>
-              <button
-                onClick={home}
-                className="lg:text-3xl md:text-xl h-fit text-neutral-secondary"
-              >
-                Submit
-              </button>
-            </div>
-            <span className="grid items-end justify-center pt-20 cursor-pointer">
-              <img src={StarImg} alt="StarImg" />
-            </span>
-            <div className="grid grid-cols-2 gap-10 pt-20">
-              <div className="grid items-end justify-center">
-                <span className="grid items-center justify-center">
-                  <img
-                    src={CameraImg}
-                    alt="CameraImg"
-                    className="w-full h-full"
-                  />
-                </span>
-                <button className="mt-10 text-center xl:text-5xl lg:text-4xl md:text-2xl text-primary-default">
-                  Add Photo
+              <span className="grid items-end justify-center pt-6">
+                <button className="px-20 text-xl capitalize text-light-default dark:text-dark-default btn btn-primary">
+                  Submit
                 </button>
-              </div>
-              <div className="grid items-end justify-center">
-                <span className="grid items-center justify-center">
-                  <img
-                    src={VideoImg}
-                    alt="VideoImg"
-                    className="w-full h-full"
-                  />
-                </span>
-                <button className="mt-10 text-center xl:text-5xl lg:text-4xl md:text-2xl text-primary-default">
-                  Add Video
-                </button>
-              </div>
+              </span>
             </div>
           </div>
         </div>
-      </div>
+      </Card>
     </>
   );
 }
