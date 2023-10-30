5313410af504e26393d4235e2ced5b24b7723bbc
Jay200123 <kenken.ayonon@gmail.com>
2023-10-30 19:11:13 +0800

    about commit

---
+++ src/components/card/AboutCards/CardImage3.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CardImg from "@/assets/salon-hd2.png"
+
+export default function(){
+    return(
+        <>
+        <img className="w-full m-0" src={CardImg} alt="Lhanlee Salon"/>
+        </>
+    )
+}
