ee994373f747ea9428da2678dc2508cf21a4310b
Jay200123 <kenken.ayonon@gmail.com>
2023-10-31 20:53:41 +0800

    profile page

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,81 @@
+import React from "react";
+import profileImg from "@/assets/Customer.png";
+
+export default function (){
+    return(
+        <>
+        <div className="flex">
+        <div class="w-56 bg-transparent h-screen text-black rounded w-64 h-screen bg-gray-200 shadow-lg bg-opacity-75 p-4 mt-2">
+            <div className="flex items-center m-5">
+            <img className="border rounded-full w-11 h-11 border-pink-300 m-1" src={profileImg} alt="User Profile"/>
+            <div class="p-4 text-base font-semibold">Juan Dela Cruz</div>
+            </div>
+    
+        <ul class="space-y-2 text-center text-base">
+        <li class="p-2 group relative">
+        <a href="#" class="hover:text-pink-700"><i className="fa fa-user-circle" aria-hidden="true"></i>My Account</a>
+            </li>
+            <li class="p-2"><a href="#" class="hover:text-pink-700"><i className="fa fa-history" aria-hidden="true"></i>History</a></li>
+            <li class="p-2"><a href="#" class="hover:text-pink-700"><i className="fa fa-calendar" aria-hidden="true"></i>Schedule</a></li>
+             <li class="p-2"><a href="#" class="hover:text-pink-700"><i className="fa fa-heart" aria-hidden="true"></i>My Favorites</a></li>
+        </ul>
+    </div>
+
+    <div class="flex-1 p-4 w-full h-full bg-white m-20 p-5 text-black rounded-lg shadow-lg flex flex-col md:flex-row items-center space-x-4">
+    <div class="md:w-3/4">
+        <h2 class="text-3xl font-sans font-extrabold mb-4">My Profile</h2>
+        <h5 className="font-medium ">Manage and protect your own account</h5>
+        <h3 className="border-t border-gray-300"></h3>
+        <div class="pt-4">
+            <div class="flex items-center space-x-2 mt-4 ">
+                <div class="w-1/3 text-gray-500">User Name:</div>
+                <div class="w-2/3 font-semibold">YourUserName123</div>
+            </div>    
+        </div>
+        <div class="pt-4">
+            <div class="flex items-center space-x-2 mt-4">
+                <div class="w-1/3 text-gray-500">First Name:</div>
+                <div class="w-2/3 font-semibold">Juan</div>
+            </div>    
+        </div>
+        <div class="pt-4">
+            <div class="flex items-center space-x-2 mt-4">
+                <div class="w-1/3 text-gray-500">Last Name:</div>
+                <div class="w-2/3 font-semibold">Dela Cruz</div>
+            </div>    
+        </div>
+        <div class="pt-4">
+            <div class="flex items-center space-x-2 mt-4">
+                <div class="w-1/3 text-gray-500">Phone:</div>
+                <div class="w-2/3 font-semibold">09123456789</div>
+            </div>    
+        </div>
+        <div class="pt-4">
+            <div class="flex items-center space-x-2 mt-4">
+                <div class="w-1/3 text-gray-500">Email:</div>
+                <div class="w-2/3 font-semibold">juan.delacruz@gmail.com</div>
+            </div>    
+        </div>
+        <div class="pt-4">
+            <div class="flex items-center space-x-2 mt-4">
+                <div class="w-1/3 text-gray-500">Birth Date:</div>
+                <div class="w-2/3 font-semibold">10-23-2001</div>
+            </div>    
+        </div>
+    </div>
+    <div class="mt-4 md:mt-0 md:w-1/4">
+    <div class="flex flex-col items-center space-x-2">
+        <img src={profileImg} alt="Profile Picture" class="w-42 h-42 rounded-full border border-gray-300" />
+        <h3 class="text-gray-600 text-center text-xl font-semibold mt-1.5">User Profile</h3>
+        <button class="bg-pink-500 text-white px-4 py-2 rounded-full mt-2">Edit Profile</button>
+
+    </div>
+</div>
+</div>
+
+
+
+        </div>
+        </>
+    )
+}
