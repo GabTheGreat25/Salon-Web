0732bab8385d1302032d1b58e123cb20320a7434
Jay200123 <kenken.ayonon@gmail.com>
2023-11-02 20:22:48 +0800

    data commit

---
+++ src/data/history.js
@@ -0,0 +1,33 @@
+import image from "@/assets/Logo-3.png";
+
+const history = [
+    {
+        id:1,
+        date: "01 Jan 2023",
+        description:"Face Wash",
+        variation:"Face Service",
+        price:"1,300.00",
+        status:"completed",
+        serviceImg:image
+    },
+    {
+        id:2,
+        date: "02 Jan 2023",
+        description:"Face Wash",
+        variation:"Face Service",
+        price:"1,300.00",
+        status:"pending",
+        serviceImg:image
+    },
+    {
+        id:3,
+        date: "03 Jan 2023",
+        description:"Face Wash",
+        variation:"Face Service",
+        price:"1,300.00",
+        status:"pending",
+        serviceImg:image
+    }
+]
+
+export default history
