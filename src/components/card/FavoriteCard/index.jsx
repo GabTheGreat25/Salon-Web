9fc91ac57713157c49bbe7dc766e6be2fe8d0b5f
Jay200123 <kenken.ayonon@gmail.com>
2023-11-02 21:17:28 +0800

    favorites commit

---
+++ src/components/card/FavoriteCard/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+export default function (data) {
+    console.log(data)
+    return(
+        <>
+            <div class="bg-white rounded-lg shadow-md">
+                <img src={data.image} alt="Image 1" class="h-52 w-46 m-1.5 w-full object-cover rounded-t-lg" /> 
+                <div className="flex items-center">
+                <div class="p-4">
+                    <h2 class="text-xl font-semibold">{data.description}</h2>
+                    <p class="text-pink-300 font-semibold">Price:<span>{data.price}</span></p>
+                </div>
+                <button className="ml-auto border border-pink-500 text-pink-500 hover:bg-pink-100 h-10 w-20 text-sm m-1">Find Similar</button>
+                </div>
+            </div>
+        </>
+    )
+}
