88281ee8cff103c180c1932f164fbb804f4a10fc
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-12 18:27:40 +0800

    Fix FacebookMessenger showing in mobile mode

--- src/components/FacebookMessenger/index.jsx
+++ src/components/FacebookMessenger/index.jsx
@@ -1,13 +1,19 @@
-"use client";
 import React from "react";
 import { FacebookProvider, CustomChat } from "react-facebook";
+import { useMediaQuery } from "react-responsive";
+
+const MOBILE_BREAKPOINT = 767;
 
 export default function () {
+  const isMobile = useMediaQuery({ maxWidth: MOBILE_BREAKPOINT });
+
   return (
     <>
-      <FacebookProvider appId="625328572915568" chatSupport>
-        <CustomChat pageId="127317223684561" minimized="true" />
-      </FacebookProvider>
+      {isMobile ? null : (
+        <FacebookProvider appId="625328572915568" chatSupport>
+          <CustomChat pageId="127317223684561" minimized="true" />
+        </FacebookProvider>
+      )}
     </>
   );
 }
