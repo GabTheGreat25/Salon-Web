306093da922218d1804f678a12c71d353dc3cd2f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 13:45:46 +0800

    Add store persist and toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { Loader } from "@/components";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistor } from "./state/store";
+import { ToastContainer } from "react-toastify";
+import { PersistGate } from "redux-persist/integration/react";
 import { RESOURCE } from "@/constants";
+import { Loader } from "@/components";
 
 function Root() {
   const [loading, setLoading] = useState(true);
@@ -14,7 +18,22 @@ function Root() {
     }, 3000);
   }, []);
   //! Development mode use this when you want to see the loader
-  return <React.StrictMode>{loading ? <Loader /> : <App />}</React.StrictMode>;
+  return (
+    <React.StrictMode>
+      {loading ? (
+        <Loader />
+      ) : (
+        <React.StrictMode>
+          <Provider store={store}>
+            <PersistGate persistor={persistor}>
+              <ToastContainer />
+              <App />
+            </PersistGate>
+          </Provider>
+        </React.StrictMode>
+      )}
+    </React.StrictMode>
+  );
   // return (
   //   <React.StrictMode>
   //     <App />
