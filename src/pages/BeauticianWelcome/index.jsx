ac394915432073d4dcaa70a786f28f5c31ac2b89
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 23:44:30 +0800

    Add BeauticianWelcome

---
+++ src/pages/BeauticianWelcome/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function () {
+  return (
+    <>
+      <div className="grid items-center justify-center h-screen">
+        <h1>Beautician Login Works!</h1>
+      </div>
+    </>
+  );
+}
