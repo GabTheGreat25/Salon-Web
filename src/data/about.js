286df940c7c2bf085516c03bd63ef13fa54868c5
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 13:46:55 +0800

    Change employee to beautician in about

--- src/data/about.js
+++ src/data/about.js
@@ -3,24 +3,24 @@ import image2 from "@/assets/johndoe.png";
 import image3 from "@/assets/Logo-3.png";
 
 const about = [
-    {
-        id:1,
-        title:"Well Trained",
-        description: "Lhanlee Employees",
-        image:image1
-    },
-    {
-        id:2,
-        title:"Our Customer",
-        description:"Is Our First Priority",
-        image:image2
-    },
-    {
-        id:3,
-        title:"Affordable Prices",
-        description:"With Our Services",
-        image:image3
-    }
-]
+  {
+    id: 1,
+    title: "Well Trained",
+    description: "Lhanlee Beauticians",
+    image: image1,
+  },
+  {
+    id: 2,
+    title: "Our Customer",
+    description: "Is Our First Priority",
+    image: image2,
+  },
+  {
+    id: 3,
+    title: "Affordable Prices",
+    description: "With Our Services",
+    image: image3,
+  },
+];
 
-export default about
+export default about;
