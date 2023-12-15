a4a0626ec71a21afb7bbd9657219442739c5af0f
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 13:46:26 +0800

    Add route constants

---
+++ src/constants/route.js
@@ -0,0 +1,9 @@
+export default {
+  LOGIN_ROUTE: "/login",
+  LOGOUT_ROUTE: "/logout",
+  USERS_ROUTE: "users",
+  USER_ID_ROUTE: "user/:id",
+  EDIT_USER_ID_ROUTE: "user/edit/:id",
+  UPDATE_PASSWORD_ROUTE: "/updatePassword/:id",
+  ACTIVATE_USER_ID_ROUTE: "/user/activate/:id",
+};
