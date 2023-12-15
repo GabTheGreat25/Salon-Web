902839c5b55b4e93a9434d322335470aa05879f3
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 23:42:05 +0800

    Import AdminLayout, BeauticianLayout, OnlineCustomerLayout, and WalkInCustomerLayout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,5 +4,20 @@ import Welcome from "./Welcome";
 import WelcomeTwo from "./WelcomeTwo";
 import WelcomeThree from "./WelcomeThree";
 import MainLayout from "./MainLayout";
+import AdminLayout from "./AdminLayout";
+import BeauticianLayout from "./BeauticianLayout";
+import OnlineCustomerLayout from "./OnlineCustomerLayout";
+import WalkInCustomerLayout from "./WalkInCustomerLayout";
 
-export { RootLayout, NotFound, Welcome, MainLayout, WelcomeTwo, WelcomeThree };
+export {
+  RootLayout,
+  NotFound,
+  Welcome,
+  MainLayout,
+  WelcomeTwo,
+  WelcomeThree,
+  AdminLayout,
+  BeauticianLayout,
+  OnlineCustomerLayout,
+  WalkInCustomerLayout,
+};
