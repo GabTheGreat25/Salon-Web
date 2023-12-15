f4e259397931973f6aa54277e380c6d0869605d6
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 21:59:46 +0800

    Add extra role

--- src/pages/ChooseRole/index.jsx
+++ src/pages/ChooseRole/index.jsx
@@ -7,22 +7,30 @@ export default function () {
   return (
     <Card>
       <div className="grid items-center justify-center w-full h-full text-light-default dark:text-dark-default">
-        <div className="relative grid w-full grid-cols-2 gap-4 xl:bottom-10 md:bottom-5 h-fit">
+        <div className="relative grid w-full grid-cols-3 gap-4 xl:bottom-10 md:bottom-5 h-fit">
           <Role
             image={BeauticianImg}
             imageName="BeauticianImg"
             title="Become Our Lanlee Beautician"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt minima ab harum alias aliquam."
             buttonTitle="Register as Beautician"
-            navigateTo="/BeauticianSignUp"
+            navigateTo="/beauticianSignUp"
           />
           <Role
             image={CustomerImg}
             imageName="CustomerImg"
-            title="Become Our Lanlee Customer"
+            title="Become Our Lanlee Online Customer"
             description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt minima ab harum alias aliquam."
-            buttonTitle="Register as Customer"
-            navigateTo="/CustomerSignUp"
+            buttonTitle="Register as Online Customer"
+            navigateTo="/onlineCustomerSignUp"
+          />
+          <Role
+            image={CustomerImg}
+            imageName="CustomerImg"
+            title="Become Our Lanlee Walk-In Customer"
+            description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut incidunt minima ab harum alias aliquam."
+            buttonTitle="Register as Walk-In Customer"
+            navigateTo="/walkInCustomerSignUp"
           />
         </div>
       </div>
