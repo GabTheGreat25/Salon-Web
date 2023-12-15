8296f450d31b396f13bc40196097e1e274b44e7a
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 13:50:09 +0800

    Import validation

---
+++ src/validation/index.js
@@ -0,0 +1,4 @@
+import createBeauticianValidation from "./createBeautician";
+import createCustomerValidation from "./createCustomer";
+
+export { createBeauticianValidation, createCustomerValidation };
