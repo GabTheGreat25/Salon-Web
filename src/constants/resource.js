32ecce430fa0b7d1115f49e20267f45fbb4086a9
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 13:46:17 +0800

    Update resource constants

--- src/constants/resource.js
+++ src/constants/resource.js
@@ -1,3 +1,11 @@
 export default {
   ROOT: "root",
+  ID: "_id",
+  INCLUDE: "include",
+  PRODUCTION: "production",
+  PHP: "PHP",
+  CONFIRM: "Are you sure?",
+  PENDING: "pending",
+  CANCELLED: "cancelled",
+  LOGOUT: "Logout successful!",
 };
