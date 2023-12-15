d2a49faa655ecd6111bf066542cceb481d30c9b4
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-15 23:44:41 +0800

    Add CustomerWelcome

---
+++ src/pages/CustomerWelcome/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function () {
+  return (
+    <>
+      <div className="grid items-center justify-center h-screen">
+        <h1>Customer Login Works!</h1>
+      </div>
+    </>
+  );
+}
