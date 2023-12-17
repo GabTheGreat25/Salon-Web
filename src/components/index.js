e74df3e17586a7d8aae855f1cbdb473522fd0a21
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-17 15:16:23 +0800

    Remove unuse imports

--- src/components/index.js
+++ src/components/index.js
@@ -3,16 +3,11 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 import Loader from "./Loader";
 import Card from "./card";
-import SignUp from "./SignUp";
 import Role from "./Role";
 import Welcome from "./Welcome";
 import MobileChecker from "./MobileChecker";
 import ImagePreview from "./ImagePreview";
 import CardImage from "./card/CardImage";
-import CardImage2 from "./card/AboutCards/CardImage2";
-import CardImage3 from "./card/AboutCards/CardImage3";
-import CardImage4 from "./card/AboutCards/CardImage4";
-import CardImage5 from "./card/AboutCards/CardImage5";
 import Sidebar from "./Sidebar";
 import HistoryCard from "./card/HistoryCard";
 import FavoriteCard from "./card/FavoriteCard";
@@ -30,16 +25,11 @@ export {
   Footer,
   Loader,
   Card,
-  SignUp,
   Role,
   Welcome,
   MobileChecker,
   ImagePreview,
   CardImage,
-  CardImage2,
-  CardImage3,
-  CardImage4,
-  CardImage5,
   Sidebar,
   HistoryCard,
   FavoriteCard,
