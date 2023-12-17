10d0b0cf4a9cc63b5557494b9e61f159b61c5cfc
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-17 23:21:52 +0800

    Change route to login

--- src/layouts/Welcome.jsx
+++ src/layouts/Welcome.jsx
@@ -10,7 +10,7 @@ export default function () {
         title={`Looking for a <br /> Salon?`}
         description={`Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit. Ut erat mauris, rhoncus br non <br /> nibh in, commodo viverra elit.`}
         buttonTitle="Learn More"
-        navigateTo="/asdasd"
+        navigateTo="/login"
         logo={Logo1}
         logoTitle="Logo1"
         rightArrow={true}
