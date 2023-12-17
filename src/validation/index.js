8af2c5247702d1c2edbc0a8346c9457604639e9c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-17 23:22:57 +0800

    Import editAdminValidation

--- src/validation/index.js
+++ src/validation/index.js
@@ -1,9 +1,11 @@
 import createBeauticianValidation from "./createBeautician";
 import createCustomerValidation from "./createCustomer";
 import loginUserValidation from "./loginUser";
+import editAdminValidation from "./editAdmin";
 
 export {
   createBeauticianValidation,
   createCustomerValidation,
   loginUserValidation,
+  editAdminValidation,
 };
