8e63bc6e3a5c97ed9751c9d3ad42c3ca6973ce80
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-17 15:17:29 +0800

    Refactor TermsAndConditions

--- src/pages/TermsAndConditions/index.jsx
+++ src/pages/TermsAndConditions/index.jsx
@@ -7,234 +7,44 @@ export default function () {
   };
   return (
     <>
-      <div className="items-center justify-center h-full px-10 pb-5">
+      <div className="items-center justify-center h-full px-10 py-5">
         <button onClick={goBack}>
           <FaArrowLeft size={30} />
         </button>
-        <h1 className="pb-6 text-3xl font-bold text-center">
-          Terms and Conditions
+        <h1 className="pb-6 text-6xl font-bold text-center">
+          Terms And Conditions
         </h1>
-        <h3 className="text-xl font-bold text-justify">
-          Lorem ipsum quisque duis velit sit nullam turpis curae placerat
-          laoreet, egestas risus suspendisse malesuada conubia semper praesent
-          velit per, sociosqu hac rutrum luctus sollicitudin eget vehicula
-          turpis rhoncus. Vestibulum ad lectus condimentum viverra gravida
-          potenti etiam gravida ultricies, ut nulla platea duis quam tincidunt
-          amet euismod, netus gravida lacinia integer etiam euismod fermentum
-          duis. Morbi curabitur eu hendrerit nam aptent netus, iaculis aenean
-          dolor id consectetur vivamus, netus lacinia class morbi hendrerit,
-          integer tempor posuere ut ultricies.
-          <br />
-          <br />
-          Bibendum id lorem suscipit mauris sit luctus porttitor tempus dui, ut
-          fringilla sit curabitur venenatis ullamcorper nullam in duis, dictumst
-          cursus tellus arcu gravida nibh mi elementum. Fringilla dui venenatis
-          maecenas elit arcu metus hac, placerat integer conubia vivamus dapibus
-          congue, a sodales proin etiam tellus aenean. Integer elementum
-          inceptos donec purus quis lectus conubia venenatis donec sit, pulvinar
-          class mollis curae scelerisque cras sociosqu elit curabitur sapien,
-          senectus semper pharetra lobortis egestas lorem pharetra sollicitudin
-          tincidunt.
-          <br />
-          <br />
-          Felis eget in fermentum primis congue auctor dictum dictumst per
-          aliquet, orci donec tempor et porta rutrum tortor porttitor massa mi,
-          conubia neque phasellus sem morbi habitant quam donec fames. Non
-          tellus libero consequat sagittis aenean litora erat, nisl dolor eu
-          fermentum habitant sit, nullam pellentesque nisi consectetur platea
-          arcu. Congue cubilia primis bibendum quam ornare semper, curabitur
-          vivamus condimentum integer nam eu, posuere per amet cras nisl,
-          tincidunt egestas ut sem vivamus.
-          <br />
-          <br />
-          Vehicula potenti platea ullamcorper sagittis leo donec facilisis
-          feugiat, vivamus nisl magna eu sem ipsum potenti sodales, ultricies
-          risus lacinia habitasse etiam iaculis non. Pretium maecenas arcu
-          sapien phasellus himenaeos id euismod nulla tristique, bibendum
-          accumsan leo lorem curabitur pretium velit netus litora, aenean etiam
-          molestie erat bibendum vivamus nam mi. Dictum aenean tincidunt orci mi
-          donec suspendisse senectus massa, mollis ultrices dapibus vel
-          vulputate nam porta nam sociosqu, justo metus netus eleifend libero
-          vitae porttitor.
-          <br />
-          <br />
-          Cras pharetra nunc arcu purus velit mi et, blandit nisl magna integer
-          est suspendisse nostra diam, habitant duis nec bibendum sociosqu
-          ligula. Lectus eros commodo sem sociosqu rutrum dolor senectus, hac
-          praesent lectus aliquet eu nisi primis egestas, varius commodo risus
-          praesent aenean fames. Mollis pretium convallis erat feugiat mauris
-          quisque facilisis, erat placerat vestibulum quisque elementum platea
-          sagittis, donec lacinia libero adipiscing at fusce.
-          <br />
-          <br />
-          Dictumst ultrices sit commodo vel viverra aliquam ut ultrices sociosqu
-          ultricies rhoncus curabitur fames curabitur nostra litora ligula, sed
-          mattis sem iaculis pretium ipsum semper elementum cursus sollicitudin
-          at adipiscing mattis maecenas aenean. Quisque nec quisque elit morbi
-          tempus auctor aliquam risus, pharetra morbi aenean nisl semper rhoncus
-          cras adipiscing, ut at ac aliquet nulla in purus. Venenatis maecenas
-          nulla sapien morbi consectetur laoreet suspendisse eros, iaculis
-          quisque habitant fames suspendisse habitasse hac etiam, nam consequat
-          donec congue habitant feugiat non.
-          <br />
-          <br />
-          Luctus dapibus et accumsan ut platea odio nec pretium, proin habitant
-          mollis feugiat sagittis bibendum iaculis. Habitant phasellus per duis
-          nulla nullam dolor lobortis primis justo, interdum ut eu ante tortor
-          dictum lobortis curabitur libero, tincidunt porta sagittis lorem nec
-          at laoreet leo. Quis enim nostra ultrices ad lacus habitasse magna
-          semper, conubia adipiscing id tristique tempus nulla volutpat est
-          venenatis, porta tristique habitasse magna nibh vivamus nec.
-          <br />
-          <br />
-          Sapien magna risus dapibus morbi sociosqu lacus torquent vulputate
-          pulvinar sollicitudin dolor sociosqu himenaeos suscipit sodales curae
-          etiam, leo laoreet praesent vivamus molestie mi leo sem aliquam tempus
-          enim vitae nulla curae magna. Proin consectetur ultrices molestie
-          dictum orci libero sem enim, ac litora netus vehicula odio nullam
-          tincidunt ligula porttitor, varius netus risus accumsan quisque mauris
-          interdum. Nibh aliquam quisque iaculis habitant vel justo, nec at
-          commodo cras accumsan himenaeos taciti, nam non lectus nam ultrices.
-          <br />
-          <br />
-          Bibendum scelerisque sed fusce pretium vel sem interdum, nisi senectus
-          vehicula urna nibh nulla libero, elit porttitor nostra cras ad
-          ultrices. Ut massa quisque vivamus purus fames pretium pulvinar, quam
-          venenatis morbi blandit in turpis sed facilisis, etiam conubia
-          venenatis malesuada odio sed. Sollicitudin luctus nunc eu hac feugiat
-          ante aliquam, massa purus a himenaeos laoreet lectus, aenean ad
-          venenatis rhoncus erat sit.
-          <br />
-          <br />
-          Sociosqu lorem consequat faucibus rutrum ligula eleifend id litora
-          leo, pharetra elementum venenatis massa elementum torquent etiam
-          iaculis magna, nulla cursus dictum ac condimentum etiam ante tortor.
-          Euismod ipsum eleifend orci potenti cubilia eros himenaeos nostra
-          metus vehicula habitant varius blandit, sem gravida ullamcorper
-          porttitor iaculis nunc gravida curae eleifend aliquet lectus. Leo vel
-          lobortis conubia ut varius congue inceptos et, lorem eleifend
-          tristique turpis dictum suscipit lorem fames, at elementum platea
-          dictum tristique eu suspendisse.
-          <br />
-          <br />
-          Bibendum sem quisque at nisi eleifend fermentum, mollis habitasse
-          mattis integer at aenean, conubia arcu mi hac rhoncus. Sodales et quam
-          justo quam mollis odio ligula cras, dapibus ultricies ut accumsan
-          convallis lorem semper, phasellus ad faucibus aliquet cursus primis
-          lacinia. Adipiscing elit semper eros amet magna class, tempor lectus
-          ipsum feugiat aenean nisl senectus, turpis praesent scelerisque dui
-          consectetur. Potenti vel hac lacinia, phasellus faucibus.
-          <br />
-          <br />
-          Lorem ipsum quisque duis velit sit nullam turpis curae placerat
-          laoreet, egestas risus suspendisse malesuada conubia semper praesent
-          velit per, sociosqu hac rutrum luctus sollicitudin eget vehicula
-          turpis rhoncus. Vestibulum ad lectus condimentum viverra gravida
-          potenti etiam gravida ultricies, ut nulla platea duis quam tincidunt
-          amet euismod, netus gravida lacinia integer etiam euismod fermentum
-          duis. Morbi curabitur eu hendrerit nam aptent netus, iaculis aenean
-          dolor id consectetur vivamus, netus lacinia class morbi hendrerit,
-          integer tempor posuere ut ultricies.
-          <br />
-          <br />
-          Bibendum id lorem suscipit mauris sit luctus porttitor tempus dui, ut
-          fringilla sit curabitur venenatis ullamcorper nullam in duis, dictumst
-          cursus tellus arcu gravida nibh mi elementum. Fringilla dui venenatis
-          maecenas elit arcu metus hac, placerat integer conubia vivamus dapibus
-          congue, a sodales proin etiam tellus aenean. Integer elementum
-          inceptos donec purus quis lectus conubia venenatis donec sit, pulvinar
-          class mollis curae scelerisque cras sociosqu elit curabitur sapien,
-          senectus semper pharetra lobortis egestas lorem pharetra sollicitudin
-          tincidunt.
-          <br />
-          <br />
-          Felis eget in fermentum primis congue auctor dictum dictumst per
-          aliquet, orci donec tempor et porta rutrum tortor porttitor massa mi,
-          conubia neque phasellus sem morbi habitant quam donec fames. Non
-          tellus libero consequat sagittis aenean litora erat, nisl dolor eu
-          fermentum habitant sit, nullam pellentesque nisi consectetur platea
-          arcu. Congue cubilia primis bibendum quam ornare semper, curabitur
-          vivamus condimentum integer nam eu, posuere per amet cras nisl,
-          tincidunt egestas ut sem vivamus.
-          <br />
-          <br />
-          Vehicula potenti platea ullamcorper sagittis leo donec facilisis
-          feugiat, vivamus nisl magna eu sem ipsum potenti sodales, ultricies
-          risus lacinia habitasse etiam iaculis non. Pretium maecenas arcu
-          sapien phasellus himenaeos id euismod nulla tristique, bibendum
-          accumsan leo lorem curabitur pretium velit netus litora, aenean etiam
-          molestie erat bibendum vivamus nam mi. Dictum aenean tincidunt orci mi
-          donec suspendisse senectus massa, mollis ultrices dapibus vel
-          vulputate nam porta nam sociosqu, justo metus netus eleifend libero
-          vitae porttitor.
-          <br />
-          <br />
-          Cras pharetra nunc arcu purus velit mi et, blandit nisl magna integer
-          est suspendisse nostra diam, habitant duis nec bibendum sociosqu
-          ligula. Lectus eros commodo sem sociosqu rutrum dolor senectus, hac
-          praesent lectus aliquet eu nisi primis egestas, varius commodo risus
-          praesent aenean fames. Mollis pretium convallis erat feugiat mauris
-          quisque facilisis, erat placerat vestibulum quisque elementum platea
-          sagittis, donec lacinia libero adipiscing at fusce.
-          <br />
-          <br />
-          Dictumst ultrices sit commodo vel viverra aliquam ut ultrices sociosqu
-          ultricies rhoncus curabitur fames curabitur nostra litora ligula, sed
-          mattis sem iaculis pretium ipsum semper elementum cursus sollicitudin
-          at adipiscing mattis maecenas aenean. Quisque nec quisque elit morbi
-          tempus auctor aliquam risus, pharetra morbi aenean nisl semper rhoncus
-          cras adipiscing, ut at ac aliquet nulla in purus. Venenatis maecenas
-          nulla sapien morbi consectetur laoreet suspendisse eros, iaculis
-          quisque habitant fames suspendisse habitasse hac etiam, nam consequat
-          donec congue habitant feugiat non.
-          <br />
-          <br />
-          Luctus dapibus et accumsan ut platea odio nec pretium, proin habitant
-          mollis feugiat sagittis bibendum iaculis. Habitant phasellus per duis
-          nulla nullam dolor lobortis primis justo, interdum ut eu ante tortor
-          dictum lobortis curabitur libero, tincidunt porta sagittis lorem nec
-          at laoreet leo. Quis enim nostra ultrices ad lacus habitasse magna
-          semper, conubia adipiscing id tristique tempus nulla volutpat est
-          venenatis, porta tristique habitasse magna nibh vivamus nec.
-          <br />
-          <br />
-          Sapien magna risus dapibus morbi sociosqu lacus torquent vulputate
-          pulvinar sollicitudin dolor sociosqu himenaeos suscipit sodales curae
-          etiam, leo laoreet praesent vivamus molestie mi leo sem aliquam tempus
-          enim vitae nulla curae magna. Proin consectetur ultrices molestie
-          dictum orci libero sem enim, ac litora netus vehicula odio nullam
-          tincidunt ligula porttitor, varius netus risus accumsan quisque mauris
-          interdum. Nibh aliquam quisque iaculis habitant vel justo, nec at
-          commodo cras accumsan himenaeos taciti, nam non lectus nam ultrices.
-          <br />
-          <br />
-          Bibendum scelerisque sed fusce pretium vel sem interdum, nisi senectus
-          vehicula urna nibh nulla libero, elit porttitor nostra cras ad
-          ultrices. Ut massa quisque vivamus purus fames pretium pulvinar, quam
-          venenatis morbi blandit in turpis sed facilisis, etiam conubia
-          venenatis malesuada odio sed. Sollicitudin luctus nunc eu hac feugiat
-          ante aliquam, massa purus a himenaeos laoreet lectus, aenean ad
-          venenatis rhoncus erat sit.
-          <br />
-          <br />
-          Sociosqu lorem consequat faucibus rutrum ligula eleifend id litora
-          leo, pharetra elementum venenatis massa elementum torquent etiam
-          iaculis magna, nulla cursus dictum ac condimentum etiam ante tortor.
-          Euismod ipsum eleifend orci potenti cubilia eros himenaeos nostra
-          metus vehicula habitant varius blandit, sem gravida ullamcorper
-          porttitor iaculis nunc gravida curae eleifend aliquet lectus. Leo vel
-          lobortis conubia ut varius congue inceptos et, lorem eleifend
-          tristique turpis dictum suscipit lorem fames, at elementum platea
-          dictum tristique eu suspendisse.
-          <br />
-          <br />
-          Bibendum sem quisque at nisi eleifend fermentum, mollis habitasse
-          mattis integer at aenean, conubia arcu mi hac rhoncus. Sodales et quam
-          justo quam mollis odio ligula cras, dapibus ultricies ut accumsan
-          convallis lorem semper, phasellus ad faucibus aliquet cursus primis
-          lacinia. Adipiscing elit semper eros amet magna class, tempor lectus
-          ipsum feugiat aenean nisl senectus, turpis praesent scelerisque dui
-          consectetur. Potenti vel hac lacinia, phasellus faucibus.
+        <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
+          By utilizing our Salon Appointment System, you acknowledge and agree
+          to the following terms and conditions.
+        </h3>
+        <br />
+        <br />
+        <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
+          To secure your appointment, a non-refundable reservation fee of 150
+          pesos must be paid through GCash or in cash at the salon. Upon
+          successful payment, you will receive a unique QR code containing your
+          appointment details, which must be presented for verification.
+        </h3>
+        <br />
+        <br />
+        <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
+          Ensure the payment of the reservation fee is completed at least one
+          hour before your scheduled appointment time to avoid cancellation. The
+          reservation fee is non-refundable, even in the case of appointment
+          cancellation, though you may reschedule based on availability. Please
+          adhere to salon policies during your appointment, as failure to comply
+          may result in termination without refund. These terms may be modified,
+          and continued use of the system constitutes acceptance of updated
+          terms.
+        </h3>
+        <br />
+        <br />
+        <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
+          By using our Lhanlee Beauty Lounge Appointment/Management System, you
+          confirm that you have read and agree to the Salon Appointment System
+          Terms and Conditions and understand the non-refundable nature of the
+          reservation fee.
         </h3>
       </div>
     </>
