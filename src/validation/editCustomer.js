de62f0ad2c3bc93e20a246b3172051e14cbb0740
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-19 00:00:22 +0800

    Add editCustomer validation

---
+++ src/validation/editCustomer.js
@@ -0,0 +1,33 @@
+import * as yup from "yup";
+
+const isPhilippineContactNumber = (value) => {
+  const regex = /^(\+63|0|63)?[9]\d{9}$/;
+  return regex.test(value);
+};
+
+export default yup.object({
+  name: yup.string("Enter your Name").required("Name is required"),
+  age: yup
+    .number("Enter your age")
+    .required("Age is required")
+    .min(18, "Age should be of minimum 18 years"),
+  contact_number: yup
+    .string("Enter your contact number")
+    .required("Contact number is required")
+    .min(11, "Contact number should be exactly 11 digits")
+    .max(11, "Contact number should be exactly 11 digits")
+    .test(
+      "isPhilippineContactNumber",
+      "Invalid Philippine contact number",
+      isPhilippineContactNumber
+    ),
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("Email is required"),
+  description: yup
+    .string("Enter your description")
+    .required("Description is required"),
+  allergy: yup.array().min(1, "Allergy is required"),
+  product_preference: yup.array().min(1, "Product Preference is required"),
+});
