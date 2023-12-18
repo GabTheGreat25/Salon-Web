2b1ab43248a9cb6238ae5802b450d1c0e60858ce
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-18 13:06:54 +0800

    Add GoBack link

--- src/components/Sidebar/AdminSidebar.jsx
+++ src/components/Sidebar/AdminSidebar.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import { faArrowLeft, faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export default function () {
   const user = useSelector((state) => state.auth.user);
+  const navigate = useNavigate();
 
   const isActiveRoute = (route) =>
     window.location.pathname.includes(`/admin/${route}`);
+
+  const handleGoBack = () => {
+    navigate("/admin");
+  };
 
   return (
     <>
@@ -40,15 +46,28 @@ export default function () {
               Edit Profile
             </NavLink>
             <NavLink
-              to="/admin/editAdminPassword"
+              to="/admin/changePassword"
               className={`py-2 pl-6 text-center font-base ${
-                isActiveRoute("editAdminPassword")
+                isActiveRoute("changePassword")
                   ? "text-primary-default"
                   : "text-dar"
               }`}
             >
               Edit Password
             </NavLink>
+            <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
+              <div className="grid justify-center">
+                <FontAwesomeIcon icon={faArrowLeft} className="text-lg" />
+              </div>
+              <div className="grid justify-start">
+                <a
+                  onClick={handleGoBack}
+                  className={`text-lg font-light cursor-pointer`}
+                >
+                  Go Back
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
