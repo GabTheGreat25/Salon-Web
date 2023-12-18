a5dbcd344bb79ba28a7dd1dbd5b13cca0c906ac1
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-18 13:08:22 +0800

    Import UpdatePassword

--- src/validation/index.js
+++ src/validation/index.js
@@ -2,10 +2,12 @@ import createBeauticianValidation from "./createBeautician";
 import createCustomerValidation from "./createCustomer";
 import loginUserValidation from "./loginUser";
 import editAdminValidation from "./editAdmin";
+import updatePasswordValidation from "./updatePassword";
 
 export {
   createBeauticianValidation,
   createCustomerValidation,
   loginUserValidation,
   editAdminValidation,
+  updatePasswordValidation,
 };
