a1193009da9737e31436036c229fbc7fde39e0c8
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-20 23:23:36 +0800

    Refactor OnlineCustomerSidebar

--- src/components/Sidebar/OnlineCustomerSidebar.jsx
+++ src/components/Sidebar/OnlineCustomerSidebar.jsx
@@ -18,58 +18,72 @@ export default function () {
 
   return (
     <>
-      <div>
-        <div className="h-full p-4 mt-2 rounded shadow-lg w-72">
-          <div className="grid items-center justify-center pt-5">
-            <div className="p-4 overflow-hidden font-semibold capitalize lg:text-base md:text-sm whitespace-nowrap">
-              Welcome Back,
-              {user?.name.length > 10
-                ? `${user.name.slice(0, 10)}...`
-                : user.name}
+      <div className="min-h-screen p-4 mt-2 rounded shadow-lg w-72">
+        <div className="grid items-center justify-center pt-5">
+          <div className="p-4 overflow-hidden font-semibold capitalize lg:text-base md:text-sm whitespace-nowrap">
+            Welcome Back,
+            {user?.name.length > 10
+              ? `${user.name.slice(0, 10)}...`
+              : user.name}
+          </div>
+        </div>
+        <hr className="my-4 border-t border-dark-default dark:border-light-default" />
+        <div className="grid items-center justify-center grid-flow-row-dense">
+          <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
+            <div className="grid justify-end">
+              <FontAwesomeIcon icon={faUserAlt} className="text-lg" />
+            </div>
+            <div className="grid justify-center">
+              <h1 className={`text-lg font-semibold`}>My Account</h1>
             </div>
           </div>
-          <hr className="my-4 border-t border-dark-default dark:border-light-default" />
-          <div className="grid items-center justify-center grid-flow-row-dense">
-            <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
-              <div className="grid justify-center">
-                <FontAwesomeIcon icon={faUserAlt} className="text-lg" />
-              </div>
-              <div className="grid justify-center">
-                <h1 className={`text-lg font-semibold`}>My Account</h1>
-              </div>
+          <NavLink
+            to="/onlineCustomer/editOnlineCustomerProfile"
+            className={`pt-2 text-center cursor-pointer font-base ${
+              isActiveRoute("editOnlineCustomerProfile")
+                ? "text-primary-default"
+                : "text-dark-default dark:text-light-default"
+            }`}
+          >
+            Edit Profile
+          </NavLink>
+          <NavLink
+            to="/onlineCustomer/changePassword"
+            className={`py-2 pl-6 text-center font-base ${
+              isActiveRoute("changePassword")
+                ? "text-primary-default"
+                : "text-dar"
+            }`}
+          >
+            Edit Password
+          </NavLink>
+          <NavLink
+            to="/onlineCustomer/history"
+            className={`pb-2 pl-3 text-center font-base ${
+              isActiveRoute("history") ? "text-primary-default" : "text-dar"
+            }`}
+          >
+            View History
+          </NavLink>
+          <NavLink
+            to="/onlineCustomer/schedule"
+            className={`pb-2 pl-8 text-center font-base ${
+              isActiveRoute("schedule") ? "text-primary-default" : "text-dar"
+            }`}
+          >
+            View Schedule
+          </NavLink>
+          <div className="grid items-center grid-cols-[10%_auto] gap-x-4">
+            <div className="grid justify-end">
+              <FontAwesomeIcon icon={faArrowLeft} className="text-lg" />
             </div>
-            <NavLink
-              to="/onlineCustomer/editOnlineCustomerProfile"
-              className={`pt-2 text-center cursor-pointer font-base ${
-                isActiveRoute("editOnlineCustomerProfile")
-                  ? "text-primary-default"
-                  : "text-dark-default dark:text-light-default"
-              }`}
-            >
-              Edit Profile
-            </NavLink>
-            <NavLink
-              to="/onlineCustomer/changePassword"
-              className={`py-2 pl-6 text-center font-base ${
-                isActiveRoute("changePassword")
-                  ? "text-primary-default"
-                  : "text-dar"
-              }`}
-            >
-              Edit Password
-            </NavLink>
-            <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
-              <div className="grid justify-center">
-                <FontAwesomeIcon icon={faArrowLeft} className="text-lg" />
-              </div>
-              <div className="grid justify-start">
-                <a
-                  onClick={handleGoBack}
-                  className={`text-lg font-light cursor-pointer`}
-                >
-                  Go Back
-                </a>
-              </div>
+            <div className="grid justify-start">
+              <a
+                onClick={handleGoBack}
+                className={`text-lg font-light cursor-pointer`}
+              >
+                Go Back
+              </a>
             </div>
           </div>
         </div>
