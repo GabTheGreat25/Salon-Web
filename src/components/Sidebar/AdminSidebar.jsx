8b23899676cc453b3b5cad48569e8e80e54502a9
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-20 23:23:27 +0800

    Refactor AdminSidebar

--- src/components/Sidebar/AdminSidebar.jsx
+++ src/components/Sidebar/AdminSidebar.jsx
@@ -18,58 +18,56 @@ export default function () {
 
   return (
     <>
-      <div>
-        <div className="h-full p-4 mt-2 rounded shadow-lg w-72">
-          <div className="grid items-center justify-center pt-5">
-            <div className="p-4 overflow-hidden font-semibold capitalize lg:text-base md:text-sm whitespace-nowrap">
-              Welcome Back,
-              {user?.name.length > 10
-                ? `${user.name.slice(0, 10)}...`
-                : user.name}
+      <div className="min-h-screen p-4 mt-2 rounded shadow-lg w-72">
+        <div className="grid items-center justify-center pt-5">
+          <div className="p-4 overflow-hidden font-semibold capitalize lg:text-base md:text-sm whitespace-nowrap">
+            Welcome Back,
+            {user?.name.length > 10
+              ? `${user.name.slice(0, 10)}...`
+              : user.name}
+          </div>
+        </div>
+        <hr className="my-4 border-t border-dark-default dark:border-light-default" />
+        <div className="grid items-center justify-center grid-flow-row-dense">
+          <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
+            <div className="grid justify-center">
+              <FontAwesomeIcon icon={faUserAlt} className="text-lg" />
+            </div>
+            <div className="grid justify-center">
+              <h1 className={`text-lg font-semibold`}>My Account</h1>
             </div>
           </div>
-          <hr className="my-4 border-t border-dark-default dark:border-light-default" />
-          <div className="grid items-center justify-center grid-flow-row-dense">
-            <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
-              <div className="grid justify-center">
-                <FontAwesomeIcon icon={faUserAlt} className="text-lg" />
-              </div>
-              <div className="grid justify-center">
-                <h1 className={`text-lg font-semibold`}>My Account</h1>
-              </div>
+          <NavLink
+            to="/admin/editAdminProfile"
+            className={`pt-2 text-center cursor-pointer font-base ${
+              isActiveRoute("editAdminProfile")
+                ? "text-primary-default"
+                : "text-dark-default dark:text-light-default"
+            }`}
+          >
+            Edit Profile
+          </NavLink>
+          <NavLink
+            to="/admin/changePassword"
+            className={`py-2 pl-6 text-center font-base ${
+              isActiveRoute("changePassword")
+                ? "text-primary-default"
+                : "text-dar"
+            }`}
+          >
+            Edit Password
+          </NavLink>
+          <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
+            <div className="grid justify-center">
+              <FontAwesomeIcon icon={faArrowLeft} className="text-lg" />
             </div>
-            <NavLink
-              to="/admin/editAdminProfile"
-              className={`pt-2 text-center cursor-pointer font-base ${
-                isActiveRoute("editAdminProfile")
-                  ? "text-primary-default"
-                  : "text-dark-default dark:text-light-default"
-              }`}
-            >
-              Edit Profile
-            </NavLink>
-            <NavLink
-              to="/admin/changePassword"
-              className={`py-2 pl-6 text-center font-base ${
-                isActiveRoute("changePassword")
-                  ? "text-primary-default"
-                  : "text-dar"
-              }`}
-            >
-              Edit Password
-            </NavLink>
-            <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
-              <div className="grid justify-center">
-                <FontAwesomeIcon icon={faArrowLeft} className="text-lg" />
-              </div>
-              <div className="grid justify-start">
-                <a
-                  onClick={handleGoBack}
-                  className={`text-lg font-light cursor-pointer`}
-                >
-                  Go Back
-                </a>
-              </div>
+            <div className="grid justify-start">
+              <a
+                onClick={handleGoBack}
+                className={`text-lg font-light cursor-pointer`}
+              >
+                Go Back
+              </a>
             </div>
           </div>
         </div>
