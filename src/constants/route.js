d4b216d539a321d7300e2f53713702ce20baae94
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-20 11:42:03 +0800

    Add services routes constant

--- src/constants/route.js
+++ src/constants/route.js
@@ -6,4 +6,7 @@ export default {
   EDIT_USER_ID_ROUTE: "user/edit/:id",
   UPDATE_PASSWORD_ROUTE: "/updatePassword/:id",
   ACTIVATE_USER_ID_ROUTE: "/user/activate/:id",
+  SERVICES_ROUTE: "services",
+  SERVICE_ID_ROUTE: "service/:id",
+  EDIT_SERVICE_ID_ROUTE: "service/edit/:id",
 };
