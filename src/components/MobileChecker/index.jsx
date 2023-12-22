0838515f32904c3d26eee05e6a3c134c902ad9a0
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-22 23:53:15 +0800

    Refactor MobileChecker

--- src/components/MobileChecker/index.jsx
+++ src/components/MobileChecker/index.jsx
@@ -3,11 +3,13 @@ import QrCode from "@assets/qrCode.png";
 
 export default function () {
   return (
-    <div className="fixed z-50 transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-      <p className="pb-4 text-lg font-semibold text-center">
-        Please download our mobile app for a better experience.
+    <div className="fixed z-[1000] transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+      <p className="pb-4 text-xl font-semibold text-center">
+        Please download our mobile app <br /> for a better experience.
       </p>
-      <img src={QrCode} alt="qr code" className="w-64 h-64" />
+      <div className="grid items-center justify-center">
+        <img src={QrCode} alt="qr code" className="w-64 h-64 rounded-xl" />
+      </div>
     </div>
   );
 }
