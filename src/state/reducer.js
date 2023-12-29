f53427042b4e4bbb65808ba16a56264aeccd7138
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-29 22:34:36 +0800

    Add appointment reducer in root

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth/authReducer";
+import appointment from "./appointment/appointmentReducer";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth,
+  appointment,
   [api.reducerPath]: api.reducer,
 });
