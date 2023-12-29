9ebffcf18958acaaa639450585b8429e8643dc84
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-29 22:34:55 +0800

    Add appointment builder

---
+++ src/state/api/routes/appointment.js
@@ -0,0 +1,53 @@
+import { ROUTE, TAGS, API } from "@/constants";
+
+export const get = (builder) => {
+  return builder.query({
+    query: () => `${ROUTE.APPOINTMENTS_ROUTE}`,
+    method: API.GET,
+    providesTags: [TAGS.APPOINTMENTS],
+  });
+};
+
+export const getById = (builder) => {
+  return builder.query({
+    query: (id) => `${ROUTE.APPOINTMENT_ID_ROUTE.replace(":id", id)}`,
+    method: API.GET,
+    providesTags: [TAGS.APPOINTMENTS],
+  });
+};
+
+export const add = (builder) => {
+  return builder.mutation({
+    query: (payload) => ({
+      url: `${ROUTE.APPOINTMENTS_ROUTE}`,
+      method: API.POST,
+      body: payload,
+    }),
+    invalidatesTags: [TAGS.APPOINTMENTS],
+  });
+};
+
+export const updateById = (builder) => {
+  return builder.mutation({
+    query: ({ id, payload }) => {
+      return {
+        url: `${ROUTE.EDIT_APPOINTMENT_ID_ROUTE.replace(":id", id)}`,
+        method: API.PATCH,
+        body: payload,
+      };
+    },
+    invalidatesTags: [TAGS.APPOINTMENTS],
+  });
+};
+
+export const deleteById = (builder) => {
+  return builder.mutation({
+    query: (id) => ({
+      url: `${ROUTE.APPOINTMENT_ID_ROUTE.replace(":id", id)}`,
+      method: API.DELETE,
+    }),
+    invalidatesTags: [TAGS.APPOINTMENTS],
+  });
+};
+
+export default { get, getById, add, updateById, deleteById };
