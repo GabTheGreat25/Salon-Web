1f3429697a3b705fd0cd3e26f7ffe364c02f704c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-29 22:35:14 +0800

    Add state for appointmentReducer

---
+++ src/state/appointment/state.js
@@ -0,0 +1,13 @@
+export const initialState = {
+  appointmentData: [
+    {
+      service_id: "",
+      service_name: "",
+      description: "",
+      product_name: "",
+      image: [],
+      price: 0,
+      extraFee: 0,
+    },
+  ],
+};
