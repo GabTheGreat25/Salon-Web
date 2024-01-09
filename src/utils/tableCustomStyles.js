c39dfac24f099dbc44c834650668970edee13581
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-09 19:06:31 +0800

    Add tableCustomStyles

---
+++ src/utils/tableCustomStyles.js
@@ -0,0 +1,16 @@
+const tableCustomStyles = {
+  headCells: {
+    style: {
+      backgroundColor: "#FDA7DF",
+      fontSize: "14px",
+    },
+  },
+  cells: {
+    style: {
+      backgroundColor: "#FDA7DF",
+      fontSize: "14px",
+    },
+  },
+};
+
+export { tableCustomStyles };
