2f84fd878dfbf871b0746e49a8406bf713878f58
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-11 00:46:27 +0800

    Refactor editTransaction logic

--- src/pages/TransactionTable/editTransaction.jsx
+++ src/pages/TransactionTable/editTransaction.jsx
@@ -1,216 +1,122 @@
-import { React } from "react";
-import { useFormik } from "formik";
+import React from "react";
+import { Card, CardImage } from "@components";
+import { useUpdateTransactionMutation, useGetTransactionByIdQuery } from "@api";
+import { editTransactionValidation } from "@validation";
+import { useNavigate, useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { FadeLoader } from "react-spinners";
-import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { useGetTransactionByIdQuery, useUpdateTransactionMutation } from "@api";
-import { editTransactionValidation } from "@/validation";
+import { useFormik } from "formik";
 
 export default function () {
   const navigate = useNavigate();
+
+  const [updateTransaction] = useUpdateTransactionMutation();
   const { id } = useParams();
   const { data, isLoading } = useGetTransactionByIdQuery(id);
-  const [updateTransaction] = useUpdateTransactionMutation();
-  const auth = useSelector((state) => state.auth);
-  const appointment = data?.details?.appointment;
-
-  let statusDetails = ["pending", "completed"];
+  const transactions = data?.details;
 
   const formik = useFormik({
     enableReinitialize: true,
     initialValues: {
-      status: data?.details?.status || "",
-      payment: data?.details?.payment || "",
+      status: transactions?.status || "pending",
     },
     validationSchema: editTransactionValidation,
     onSubmit: async (values) => {
-      const formData = new FormData();
-      formData.append("status", values?.status);
-      formData.append("payment", values?.payment);
-      updateTransaction({ id: data?.details?._id, payload: formData }).then(
+      updateTransaction({ id: transactions._id, payload: values }).then(
         (response) => {
           const toastProps = {
             position: toast.POSITION.TOP_RIGHT,
-            autoClose: 3000,
+            autoClose: 5000,
           };
           if (response?.data?.success === true) {
-            const userRoles = auth?.user?.roles;
-            if (userRoles.includes("Admin")) {
-              navigate("/admin/transactions");
-            }
+            navigate("/admin/transactions");
             toast.success(`${response?.data?.message}`, toastProps);
-          } else {
+          } else
             toast.error(`${response?.error?.data?.error?.message}`, toastProps);
-          }
+          console.log("response", response?.error?.data?.error?.message);
         }
       );
     },
   });
+
   return (
-    <div className="min-h-screen flex items-center justify-center mb-12">
+    <>
       {isLoading ? (
         <div className="loader">
           <FadeLoader color="#FDA7DF" loading={true} size={50} />
         </div>
       ) : (
-        <div className="max-w-md w-full p-8 rounded shadow-xl bg-dark-default dark:bg-light-default">
-          <h3 className="text-white mb-2 text-center font-semibold text-xl">
-            UPDATE Transaction
-          </h3>
-          <form onSubmit={formik.handleSubmit} encType="multipart/form-data">
-            <section className="grid justify-center items-center text-center">
-              <div>
-                <label
-                  className="block text-light-default dark:text-dark-default text-sm font-bold mb-2"
-                  htmlFor="beautician"
-                >
-                  Beautician Name:
-                </label>
-                <input
-                  type="text"
-                  className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:shadow-outline dark:bg-dark-default dark:text-light-default"
-                  value={appointment?.beautician?.name}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-light-default dark:text-dark-default text-sm font-bold mb-2"
-                  htmlFor="customer"
-                >
-                  Customer Name:
-                </label>
-                <input
-                  type="text"
-                  className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:shadow-outline dark:bg-dark-default dark:text-light-default"
-                  value={appointment?.customer?.name}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-light-default dark:text-dark-default text-sm font-bold mb-2"
-                  htmlFor="customer"
-                >
-                  Appointment Date:
-                </label>
-                <input
-                  type="text"
-                  className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:shadow-outline dark:bg-dark-default dark:text-light-default"
-                  value={appointment?.date}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-light-default dark:text-dark-default text-sm font-bold mb-2"
-                  htmlFor="customer"
-                >
-                  Appointment Time:
-                </label>
-                <input
-                  type="text"
-                  className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:shadow-outline dark:bg-dark-default dark:text-light-default"
-                  value={appointment?.time}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-light-default dark:text-dark-default text-sm font-bold mb-2"
-                  htmlFor="customer"
-                >
-                  Services Cost:
-                </label>
-                <input
-                  type="text"
-                  className="w-full mb-4 px-3 py-2 border rounded focus:outline-none focus:shadow-outline dark:bg-dark-default dark:text-light-default"
-                  value={appointment?.price}
-                  readOnly
-                />
-              </div>
-              <div>
-                <label
-                  className="block text-light-default dark:text-dark-default text-sm font-bold mb-2"
-                  htmlFor="status"
-                >
-                  Transaction Status:
-                </label>
-                <select
-                  className={`w-full mb-4 px-3 py-2 border ${
-                    formik.touched.status && formik.errors.status
-                      ? "border-red-500"
-                      : "border-gray-300"
-                  } rounded focus:outline-none focus:shadow-outline dark:bg-dark-default dark:text-light-default`}
-                  id="status"
-                  name="status"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values?.status || ""}
-                >
-                  <option value="" label={formik.values?.status} />
-                  {statusDetails.map((s, index) => (
-                    <option key={index} value={s} label={s} />
-                  ))}
-                </select>
-                {formik.touched.status && formik.errors.status && (
-                  <div className="text-red-600">
-                    {formik.errors.status || ""}
-                  </div>
-                )}
-              </div>
-
-              <div>
-                <label
-                  className="block text-light-default dark:text-dark-default text-sm font-bold mb-2"
-                  htmlFor="payment"
-                >
-                  Payment Method:
-                </label>
-                <input
-                  className={`w-full mb-4 px-3 py-2 border ${
-                    formik.touched.payment && formik.errors.payment
-                      ? "border-red-500"
-                      : "border-gray-300"
-                  } rounded focus:outline-none focus:shadow-outline dark:bg-dark-default dark:text-light-default`}
-                  type="text"
-                  id="payment"
-                  name="payment"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  value={formik.values?.payment}
-                />
-                {formik.touched?.payment && formik.errors?.payment && (
-                  <div className="text-red-600">{formik.errors?.payment}</div>
-                )}
-              </div>
-
-              <span className="mt-4 grid grid-flow-col gap-x-4">
-                <button
-                  type="submit"
-                  disabled={!formik.isValid}
-                  className={`w-full bg-green-500 text-white font-bold py-2 px-4 rounded ${
-                    formik.isValid
-                      ? "hover:bg-green-700"
-                      : "cursor-not-allowed opacity-50"
-                  }`}
-                >
-                  UPDATE
-                </button>
-                <button
-                  type="button"
-                  onClick={() => navigate(-1)}
-                  className="w-full bg-blue-500 text-white font-bold py-2 px-4 rounded cursor-pointer"
-                >
-                  Go Back
-                </button>
+        <>
+          <Card>
+            <div className="grid w-full h-full text-light-default dark:text-dark-default">
+              <span className="grid items-end md:gap-y-10 justify-center 2xl:grid-rows-[90%_10%] xl:grid-rows-[80%_20%] md:grid-rows-[75%_25%]">
+                <h1 className="text-3xl font-semibold text-center">
+                  Edit Transaction
+                </h1>
+                <p className="text-xl text-center lg:px-12 text-light-default dark:text-dark-default">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Excepturi, laborum!
+                </p>
               </span>
-            </section>
-          </form>
-        </div>
+              <div className="overflow-x-hidden grid grid-cols-[50%_50%] items-center justify-start pt-20 pb-6 gap-x-6 2xl:pr-0 md:pr-10">
+                <CardImage />
+                <form
+                  onSubmit={formik.handleSubmit}
+                  className="grid justify-center w-full grid-flow-row-dense pr-12 h-1/4 gap-y-4"
+                >
+                  <label className="block">
+                    <span
+                      className={`${
+                        formik.touched.status && formik.errors.status
+                          ? "text-red-600"
+                          : "xl:text-xl lg:text-[1rem] md:text-xs font-semibold"
+                      }`}
+                    >
+                      Status:
+                    </span>
+                    <select
+                      id="status"
+                      name="status"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.status}
+                      className={` ${
+                        formik.touched.status && formik.errors.status
+                          ? "border-red-600"
+                          : "border-light-default"
+                      } block mb-2 ml-6 xl:text-lg lg:text-[1rem] placeholder-white border-0 border-b-2 bg-card-input  dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full`}
+                    >
+                      {["pending", "completed", "cancelled"].map((option) => (
+                        <option
+                          key={option}
+                          value={option}
+                          className={
+                            "text-dark-default dark:text-light-default dark:bg-dark-default font-semibold !important"
+                          }
+                        >
+                          {option}
+                        </option>
+                      ))}
+                    </select>
+                  </label>
+                  <span className="grid items-center justify-center">
+                    <button
+                      type="submit"
+                      disabled={!formik.isValid}
+                      className={`xl:px-6 md:px-4 font-medium capitalize rounded-lg xl:text-xl lg:text-[1rem] md:text-xs lg:text-base md:text-[.75rem] btn btn-primary text-light-default dark:text-dark-default ${
+                        !formik.isValid && "opacity-50 cursor-not-allowed"
+                      }`}
+                    >
+                      Submit
+                    </button>
+                  </span>
+                </form>
+              </div>
+            </div>
+          </Card>
+        </>
       )}
-    </div>
+    </>
   );
 }
