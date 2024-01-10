b96860409d9de681aa21a07dd37ea4ac5c5d0125
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-10 11:29:55 +0800

    Add FeedbackTable

---
+++ src/pages/FeedbackTable/index.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import { useGetFeedbacksQuery, useDeleteFeedbackMutation } from "@api";
+import { FaTrash } from "react-icons/fa";
+import { FadeLoader } from "react-spinners";
+import { useSelector } from "react-redux";
+import DataTable from "react-data-table-component";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { addDeletedItemId, getDeletedItemIds } from "../.././utils/DeleteItem";
+import { tableCustomStyles } from "../../utils/tableCustomStyles";
+
+export default function () {
+  const { data, isLoading } = useGetFeedbacksQuery();
+  const feedbacks = data?.details;
+
+  const [deleteFeedback, { isLoading: isDeleting }] =
+    useDeleteFeedbackMutation();
+
+  const deletedFeedbackIds = getDeletedItemIds("feedback");
+
+  const filteredFeedback = feedbacks?.filter(
+    (feedback) => !deletedFeedbackIds?.includes(feedback?._id)
+  );
+
+  const handleDelete = async (id) => {
+    if (window.confirm("Are you sure you want to delete this Feedback?")) {
+      const response = await deleteFeedback(id);
+
+      const toastProps = {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 3000,
+      };
+      if (response?.data?.success === true) {
+        toast.success(`${response?.data?.message}`, toastProps);
+        addDeletedItemId("feedback", id);
+      } else {
+        toast.error(`${response?.error?.data?.error?.message}`, toastProps);
+      }
+    }
+  };
+
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row._id,
+      sortable: true,
+    },
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Contact Number",
+      selector: (row) => row.contact_number,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+      sortable: true,
+    },
+    {
+      name: "Description",
+      selector: (row) => row.description,
+      sortable: true,
+    },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div className="text-center">
+          <FaTrash
+            className="text-xl text-red-500"
+            onClick={() => handleDelete(row._id)}
+          />
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      {isLoading || isDeleting ? (
+        <div className="mt-8 loader">
+          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+        </div>
+      ) : (
+        <div className="min-h-screen m-12 rounded-lg">
+          <DataTable
+            title="Feedbacks Table"
+            columns={columns}
+            data={filteredFeedback}
+            pagination
+            highlightOnHover
+            pointerOnHover
+            paginationPerPage={15}
+            paginationRowsPerPageOptions={[15, 30, 50]}
+            customStyles={tableCustomStyles}
+          />
+        </div>
+      )}
+    </>
+  );
+}
