9b68067142f15866eb1ea64fdce6a6561f3c1e33
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-10 23:26:56 +0800

    Add react data table in AppointmentTable

--- src/pages/AppointmentTable/index.jsx
+++ src/pages/AppointmentTable/index.jsx
@@ -1,119 +1,131 @@
 import React from "react";
 import { useGetAppointmentsQuery, useDeleteAppointmentMutation } from "@api";
-import { useNavigate } from "react-router-dom";
 import { FaEdit, FaTrash } from "react-icons/fa";
 import { FadeLoader } from "react-spinners";
+import DataTable from "react-data-table-component";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { addDeletedItemId, getDeletedItemIds } from "../.././utils/DeleteItem";
+import { tableCustomStyles } from "../../utils/tableCustomStyles";
+import { useNavigate } from "react-router-dom";
 
 export default function () {
-  const { data, isLoading, isError } = useGetAppointmentsQuery();
+  const navigate = useNavigate();
+  const { data, isLoading } = useGetAppointmentsQuery();
   const appointments = data?.details;
+
   const [deleteAppointment, { isLoading: isDeleting }] =
     useDeleteAppointmentMutation();
 
-  const navigate = useNavigate();
+  const deletedAppointmentIds = getDeletedItemIds("appointment");
+
+  const filteredAppointment = appointments?.filter(
+    (appointment) => !deletedAppointmentIds?.includes(appointment?._id)
+  );
 
   const handleDelete = async (id) => {
-    if (
-      window.confirm("Are you sure you want to delete this Appointment Record?")
-    ) {
+    if (window.confirm("Are you sure you want to delete this Appointment?")) {
       const response = await deleteAppointment(id);
+
       const toastProps = {
         position: toast.POSITION.TOP_RIGHT,
         autoClose: 3000,
       };
       if (response?.data?.success === true) {
         toast.success(`${response?.data?.message}`, toastProps);
-      } else {
+        addDeletedItemId("appointment", id);
+      } else
         toast.error(`${response?.error?.data?.error?.message}`, toastProps);
-      }
     }
   };
 
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row._id,
+      sortable: true,
+    },
+    {
+      name: "Date",
+      selector: (row) => new Date(row.date).toISOString().split("T")[0],
+      sortable: true,
+    },
+    {
+      name: "Time",
+      selector: (row) => row.time,
+      sortable: true,
+    },
+    {
+      name: "Price",
+      selector: (row) => `₱${row.price}`,
+      sortable: true,
+    },
+    {
+      name: "Extra Fee",
+      selector: (row) => `₱${row.extraFee}`,
+      sortable: true,
+    },
+    {
+      name: "Note",
+      selector: (row) => row.note || "N/A",
+      sortable: true,
+    },
+    {
+      name: "Service Name",
+      selector: (row) =>
+        Array.isArray(row.service)
+          ? row.service.map((item) => item.service_name).join(", ")
+          : row.service?.service_name,
+      sortable: true,
+    },
+    {
+      name: "Beautician",
+      selector: (row) => row.beautician?.name,
+      sortable: true,
+    },
+    {
+      name: "Customer",
+      selector: (row) => row.customer?.name,
+      sortable: true,
+    },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div className="grid grid-flow-col-dense text-center gap-x-4">
+          <FaEdit
+            className="text-xl text-blue-500"
+            onClick={() => navigate(`/admin/appointment/edit/${row._id}`)}
+          />
+          <FaTrash
+            className="text-xl text-red-500"
+            onClick={() => handleDelete(row._id)}
+          />
+        </div>
+      ),
+    },
+  ];
+
   return (
     <>
-      <div className="container mx-auto p-8">
-        {isLoading || isDeleting ? (
-          <div className="loader mt-8">
-            <FadeLoader color="#FDA7DF" loading={true} size={50} />
-          </div>
-        ) : isError ? (
-          <p className="text-center text-red-500">Error: {isError.message}</p>
-        ) : (
-          <table className="min-w-full bg-white border rounded-lg border-gray-300">
-            <thead>
-              <tr className="dark:bg-dark-default dark:text-light-default">
-                <th className="py-2 px-4 border-b text-left">
-                  Appointment Date
-                </th>
-                <th className="py-2 px-4 border-b text-left">
-                  Appointment Time
-                </th>
-                <th className="py-2 px-4 border-b text-left">
-                  Service Beautician
-                </th>
-                <th className="py-2 px-4 border-b text-left">Customer</th>
-                <th className="py-2 px-4 border-b text-left">
-                  Appointment Services
-                </th>
-                <th className="py-2 px-4 border-b text-left">Note</th>
-                <th className="py-2 px-4 border-b text-left">Extra Fee</th>
-                <th className="py-2 px-4 border-b text-left">Price</th>
-                <th className="py-2 px-4 border-b text-left">Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {appointments.map((a) => (
-                <tr key={a._id} className="dark:bg-dark-default dark:text-light-default">
-                  <td className="py-2 px-4 border-b text-left text-sm">
-                    {a.date}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left text-sm">
-                    {a.time}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left text-sm">
-                    {a.beautician.name}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left text-sm">
-                    {a.customer.name}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left">
-                    {a.service.map((s) => (
-                      <ul className="list-disc p-1 font-semibold">
-                        <li> {s.service_name}</li>
-                      </ul>
-                    ))}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left text-sm">
-                    {a.note ? a.note : "No notes"}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left text-sm">
-                    {a.extraFee}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left text-sm">
-                    {a.price}
-                  </td>
-                  <td className="py-2 px-4 border-b text-left">
-                    <div className="flex items-center space-x-4">
-                      <FaEdit
-                        className="text-blue-500 hover:cursor-pointer hover:scale-110"
-                        onClick={() =>
-                          navigate(`/admin/appointment/edit/${a._id}`)
-                        }
-                      />
-                      <FaTrash
-                        className="text-red-500 hover:cursor-pointer hover:scale-110"
-                        onClick={() => handleDelete(a._id)}
-                      />
-                    </div>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        )}
-      </div>
+      {isLoading || isDeleting ? (
+        <div className="mt-8 loader">
+          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+        </div>
+      ) : (
+        <div className="min-h-screen m-12 rounded-lg">
+          <DataTable
+            title="Appointments Table"
+            columns={columns}
+            data={filteredAppointment}
+            pagination
+            highlightOnHover
+            pointerOnHover
+            paginationPerPage={15}
+            paginationRowsPerPageOptions={[15, 30, 50]}
+            customStyles={tableCustomStyles}
+          />
+        </div>
+      )}
     </>
   );
 }
