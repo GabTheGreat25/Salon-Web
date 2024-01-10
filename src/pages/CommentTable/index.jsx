89076d9613c335029a0cdb476d0f2a967fa374fe
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-11 01:14:25 +0800

    Add CommentTable

---
+++ src/pages/CommentTable/index.jsx
@@ -0,0 +1,127 @@
+import React from "react";
+import { useGetCommentsQuery, useDeleteCommentMutation } from "@api";
+import { FaTrash, FaStar } from "react-icons/fa";
+import { FadeLoader } from "react-spinners";
+import DataTable from "react-data-table-component";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { addDeletedItemId, getDeletedItemIds } from "../.././utils/DeleteItem";
+import { tableCustomStyles } from "../../utils/tableCustomStyles";
+
+export default function () {
+  const { data, isLoading } = useGetCommentsQuery();
+  const comments = data?.details;
+
+  const [deleteComment, { isLoading: isDeleting }] = useDeleteCommentMutation();
+
+  const deletedCommentIds = getDeletedItemIds("comment");
+
+  const filteredComment = comments?.filter(
+    (comment) => !deletedCommentIds?.includes(comment?._id)
+  );
+
+  const handleDelete = async (id) => {
+    if (window.confirm("Are you sure you want to delete this Comment?")) {
+      const response = await deleteComment(id);
+
+      const toastProps = {
+        position: toast.POSITION.TOP_RIGHT,
+        autoClose: 3000,
+      };
+      if (response?.data?.success === true) {
+        toast.success(`${response?.data?.message}`, toastProps);
+        addDeletedItemId("comment", id);
+      } else
+        toast.error(`${response?.error?.data?.error?.message}`, toastProps);
+    }
+  };
+
+  const columns = [
+    {
+      name: "ID",
+      selector: (row) => row._id,
+      sortable: true,
+    },
+    {
+      name: "Ratings",
+      cell: (row) => {
+        const starCount = Math.min(5, Math.max(0, Math.floor(row.ratings)));
+        return Array.from({ length: starCount }, (_, index) => (
+          <FaStar key={index} className="text-[#feca57] text-2xl" />
+        ));
+      },
+      sortable: true,
+    },
+    {
+      name: "Description",
+      selector: (row) => row.description,
+      sortable: true,
+    },
+    {
+      name: "Suggestion",
+      selector: (row) => row.suggestion,
+      sortable: true,
+    },
+    {
+      name: "Customer",
+      selector: (row) => row?.transaction?.appointment?.customer?.name,
+      sortable: true,
+    },
+    {
+      name: "Images",
+      cell: (row) => {
+        const randomImage =
+          row.image.length > 0
+            ? row.image[Math.floor(Math.random() * row.image.length)]
+            : null;
+
+        return (
+          <div className="grid items-center justify-center">
+            {randomImage && (
+              <img
+                className="object-center w-10 h-10 rounded-full"
+                src={randomImage.url}
+                alt={randomImage.originalname}
+              />
+            )}
+          </div>
+        );
+      },
+    },
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div className="text-center">
+          <FaTrash
+            className="text-xl text-red-500"
+            onClick={() => handleDelete(row._id)}
+          />
+        </div>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      {isLoading || isDeleting ? (
+        <div className="mt-8 loader">
+          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+        </div>
+      ) : (
+        <div className="min-h-screen m-12 rounded-lg">
+          <DataTable
+            title="Comments Table"
+            columns={columns}
+            data={filteredComment}
+            pagination
+            highlightOnHover
+            pointerOnHover
+            paginationPerPage={15}
+            paginationRowsPerPageOptions={[15, 30, 50]}
+            customStyles={tableCustomStyles}
+          />
+        </div>
+      )}
+    </>
+  );
+}
