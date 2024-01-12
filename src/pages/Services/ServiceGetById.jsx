aae835f85f83d85aaec2d0eca22bf41738fcabda
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-12 20:29:55 +0800

    Add real data from state

--- src/pages/Services/ServiceGetById.jsx
+++ src/pages/Services/ServiceGetById.jsx
@@ -1,30 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { RandomServicesSidebar } from "@/components";
 import "react-toastify/dist/ReactToastify.css";
 import { FadeLoader } from "react-spinners";
-import { useGetServiceByIdQuery } from "@api";
+import { useGetServiceByIdQuery, useGetCommentsQuery } from "@api";
 import { useParams } from "react-router-dom";
-import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import {
+  faArrowLeft,
+  faStar,
+  faStarHalf,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import DummyRatings from "@assets/Rating.png";
-import Avatar from "@assets/Avatar.png";
+import noPhoto from "@/assets/no-photo.jpg";
 
 export default function () {
   const goBack = () => {
     window.history.back();
   };
 
-  const { id } = useParams();
-  const { data, isLoading } = useGetServiceByIdQuery(id);
+  const [selectedStars, setSelectedStars] = useState(5);
 
-  const { _id, service_name, description, price, image } = data?.details || {};
+  const { id } = useParams();
+  const { data: serviceData, isLoading: serviceLoading } =
+    useGetServiceByIdQuery(id);
+
+  const { data: commentsData, isLoading: commentsLoading } =
+    useGetCommentsQuery();
+  const comments = commentsData?.details || [];
+
+  const serviceComments = comments.filter((comment) =>
+    comment.transaction?.appointment?.service.some((s) => s._id === id)
+  );
+
+  const ratings = serviceComments.flatMap((comment) => comment.ratings);
+  const count = ratings.length;
+  const averageRating =
+    count > 0 ? ratings.reduce((sum, rating) => sum + rating, 0) / count : 0;
+
+  const { _id, service_name, description, price, image } =
+    serviceData?.details || {};
+
   const randomizedImages = image?.length
     ? image[Math.floor(Math.random() * image.length)].url
     : null;
 
+  const filteredServiceComments = serviceComments.filter((comment) => {
+    const commentStars = comment.ratings;
+    return commentStars === selectedStars;
+  });
+
   return (
     <>
-      {isLoading ? (
+      {serviceLoading || commentsLoading ? (
         <div className="loader">
           <FadeLoader color="#FDA7DF" loading={true} size={50} />
         </div>
@@ -53,9 +79,32 @@ export default function () {
                     <h1 className="font-semibold xl:text-3xl lg:text-xl md:text-lg">
                       {service_name}
                     </h1>
-                    <div className="grid items-center justify-center grid-cols-2 pb-10 w-fit gap-x-3">
-                      <img src={DummyRatings} alt="DummyRatings" />
-                      <p className="font-semibold">(4.6)</p>
+                    <div className="grid items-center justify-center grid-flow-col-dense grid-cols-2 pb-10 w-fit gap-x-3">
+                      <span className="grid items-center grid-flow-col-dense pt-2 text-xl w-fit gap-x-2">
+                        {averageRating > 0 ? (
+                          [...Array(Math.floor(averageRating))].map(
+                            (_, starIndex) => (
+                              <FontAwesomeIcon
+                                icon={faStar}
+                                key={starIndex}
+                                color="#feca57"
+                              />
+                            )
+                          )
+                        ) : (
+                          <>
+                            <h1>No Ratings</h1>
+                          </>
+                        )}
+                        {averageRating % 1 !== 0 && (
+                          <FontAwesomeIcon icon={faStarHalf} color="#feca57" />
+                        )}
+                        {averageRating > 0 ? (
+                          <p className="font-semibold">
+                            {averageRating} out of 5
+                          </p>
+                        ) : null}
+                      </span>
                     </div>
                     <h1 className="pb-10 font-semibold xl:text-3xl lg:text-xl md:text-lg">
                       ₱ {price}
@@ -84,64 +133,75 @@ export default function () {
                       <div className="grid gap-4 xl:grid-flow-col-dense">
                         <div className="grid grid-flow-row-dense xl:items-start xl:justify-start md:items-center md:justify-center">
                           <h1 className="xl:text-start md:text-center 2xl:text-2xl xl:text-xl lg:text-lg md:text-base">
-                            4.6 out of 5
+                            {averageRating} out of 5
                           </h1>
-                          <img
-                            src={DummyRatings}
-                            alt="DummyRatings"
-                            className="object-cover"
-                          />
                         </div>
                         <div className="grid items-center justify-center grid-flow-col-dense md:gap-x-2 xl:gap-x-4">
-                          <div className="py-2 border rounded-lg lg:px-4 md:px-2 border-primary-default hover:bg-primary-accent">
-                            <button className="2xl:text-2xl xl:text-xl lg:text-sm md:text-xs">
-                              5 Stars
-                            </button>
-                          </div>
-                          <div className="py-2 border rounded-lg lg:px-4 md:px-2 border-primary-default hover:bg-primary-accent">
-                            <button className="2xl:text-2xl xl:text-xl lg:text-sm md:text-xs">
-                              4 Stars
-                            </button>
-                          </div>
-                          <div className="py-2 border rounded-lg lg:px-4 md:px-2 border-primary-default hover:bg-primary-accent">
-                            <button className="2xl:text-2xl xl:text-xl lg:text-sm md:text-xs">
-                              3 Stars
-                            </button>
-                          </div>
-                          <div className="py-2 border rounded-lg lg:px-4 md:px-2 border-primary-default hover:bg-primary-accent">
-                            <button className="2xl:text-2xl xl:text-xl lg:text-sm md:text-xs">
-                              2 Stars
-                            </button>
-                          </div>
-                          <div className="py-2 border rounded-lg lg:px-4 md:px-2 border-primary-default hover:bg-primary-accent">
-                            <button className="2xl:text-2xl xl:text-xl lg:text-sm md:text-xs">
-                              1 Star
-                            </button>
-                          </div>
+                          {[5, 4, 3, 2, 1].map((stars) => (
+                            <div
+                              key={stars}
+                              className={`py-2 border rounded-lg lg:px-4 md:px-2 border-primary-default hover:bg-primary-accent ${
+                                selectedStars === stars
+                                  ? "bg-primary-accent"
+                                  : ""
+                              }`}
+                              onClick={() => setSelectedStars(stars)}
+                            >
+                              <button className="2xl:text-2xl xl:text-xl lg:text-sm md:text-xs">
+                                {stars} Stars
+                              </button>
+                            </div>
+                          ))}
                         </div>
                       </div>
                     </div>
-                    <div className="px-8 py-4 rounded-md bg-light-default dark:bg-dark-default">
-                      <div className="grid items-center justify-center grid-flow-col-dense w-fit gap-x-6">
-                        <div>
-                          <img src={Avatar} alt="Avatar" />
-                        </div>
-                        <div>
-                          <h1 className="font-semibold xl:text-lg md:text-base">
-                            Satisfied Customer
-                          </h1>
-                          <img src={DummyRatings} alt="DummyRatings" />
+                    {filteredServiceComments?.map((comment) => (
+                      <div
+                        key={comment._id}
+                        className="px-8 py-4 rounded-md bg-light-default dark:bg-dark-default"
+                      >
+                        <div className="grid items-center justify-center grid-flow-col-dense w-fit gap-x-6">
+                          <div>
+                            <img
+                              src={
+                                comment?.image && comment?.image.length
+                                  ? comment?.image[
+                                      Math.floor(
+                                        Math.random() * comment?.image.length
+                                      )
+                                    ]?.url
+                                  : noPhoto
+                              }
+                              alt={comment?.image?.originalname}
+                              key={comment?.image?.public_id}
+                              className="object-cover w-32 h-32 rounded-2xl"
+                            />
+                          </div>
+                          <div>
+                            <h1 className="pb-2 font-semibold xl:text-lg md:text-base">
+                              {
+                                comment?.transaction?.appointment?.customer
+                                  ?.name
+                              }
+                            </h1>
+                            <div className="flex items-center">
+                              {[...Array(comment.ratings)].map(
+                                (_, starIndex) => (
+                                  <FontAwesomeIcon
+                                    icon={faStar}
+                                    key={starIndex}
+                                    color="#feca57"
+                                  />
+                                )
+                              )}
+                            </div>
+                            <h1 className="py-4 text-lg text-justify">
+                              {comment?.description}
+                            </h1>
+                          </div>
                         </div>
                       </div>
-                      <h1 className="py-4 pl-24 text-lg text-justify">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Ut erat mauris, rhoncus non nibh in, commodo viverra
-                        elit. Nam sed faucibus dui. Suspendisse placerat
-                        venenatis dui, eget iaculis purus tincidunt vitae. Etiam
-                        semper finibus hendrerit. Sed at quam quis elit mattis
-                        vehicula quis bibendum nisi.
-                      </h1>
-                    </div>
+                    ))}
                   </div>
                 </div>
               </div>
