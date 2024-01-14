24856fcb15a727b25aa08cbf96fb80da10559231
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-14 10:06:37 +0800

    Add Loader logic

--- src/pages/Calendar/index.jsx
+++ src/pages/Calendar/index.jsx
@@ -3,14 +3,13 @@ import { useGetTransactionsQuery } from "@api";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { FadeLoader } from "react-spinners";
 
 const localizer = momentLocalizer(moment);
 
 export default function MyCalendar() {
   const { data, isLoading } = useGetTransactionsQuery();
   const transactions = data?.details || [];
-
-  console.log(transactions);
 
   const events = transactions.map((transactions) => {
     const startTime = moment(
@@ -56,68 +55,81 @@ export default function MyCalendar() {
     };
   }, [selectedEvent]);
 
-  console.log(selectedEvent);
   return (
     <>
-      <div className="rounded-2xl h-[1000px] m-10 px-2 py-10 bg-primary-default">
-        <Calendar
-          localizer={localizer}
-          events={events}
-          startAccessor="start"
-          endAccessor="end"
-          onSelectEvent={handleSelectEvent}
-          popup
-        />
-      </div>
-
-      {selectedEvent && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-[999]">
-          <div className="fixed w-full h-full opacity-75 bg-neutral-primary"></div>
-          <div className="bg-light-default dark:bg-dark-default py-6 px-12 text-justify rounded-lg shadow-lg z-[1000] w-[30rem]">
-            <div className="text-xl text-dark-default dark:text-light-default">
-              <p>
-                <span className="font-semibold">Customer:</span>{" "}
-                {selectedEvent.transactionsData?.appointment?.customer?.name}
-              </p>
-              <p>
-                <span className="font-semibold">Services:</span>{" "}
-                {selectedEvent.transactionsData?.appointment?.service
-                  .map((s) => s?.service_name)
-                  .join(", ")}
-              </p>
-              <p>
-                <span className="font-semibold">Beautician:</span>{" "}
-                {selectedEvent.transactionsData?.appointment?.beautician?.name}
-              </p>
-            </div>
-            <div className="mt-4 text-xl">
-              <p className="font-semibold">
-                Start Time: {moment(selectedEvent.start).format("hh:mm A")}
-              </p>
-              <p className="font-semibold">
-                End Time: {moment(selectedEvent.end).format("hh:mm A")}
-              </p>
-            </div>
-            <h1 className="pt-4 text-lg font-semibold text-center">
-              Copy Of Customer's Receipt
-            </h1>
-            <div className="grid items-center justify-center pt-2">
-              <img
-                src={selectedEvent.transactionsData?.qrCode}
-                alt="qr code"
-                className="w-48 h-48 rounded-xl"
-              />
-            </div>
-            <span className="grid items-center justify-center">
-              <button
-                onClick={handleCloseModal}
-                className="w-full px-20 py-2 mt-6 border rounded border-primary-accent text-dark-default dark:text-light-default hover:bg-primary-default focus:outline-none"
-              >
-                Close
-              </button>
-            </span>
-          </div>
+      {isLoading ? (
+        <div className="loader">
+          <FadeLoader color="#FDA7DF" loading={true} size={50} />
         </div>
+      ) : (
+        <>
+          <div className="rounded-2xl h-[1000px] m-10 px-2 py-10 bg-primary-default">
+            <Calendar
+              localizer={localizer}
+              events={events}
+              startAccessor="start"
+              endAccessor="end"
+              onSelectEvent={handleSelectEvent}
+              popup
+            />
+          </div>
+
+          {selectedEvent && (
+            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-[999]">
+              <div className="fixed w-full h-full opacity-75 bg-neutral-primary"></div>
+              <div className="bg-light-default dark:bg-dark-default py-6 px-12 text-justify rounded-lg shadow-lg z-[1000] w-[30rem]">
+                <div className="text-xl text-dark-default dark:text-light-default">
+                  <p>
+                    <span className="font-semibold">Customer:</span>{" "}
+                    {
+                      selectedEvent.transactionsData?.appointment?.customer
+                        ?.name
+                    }
+                  </p>
+                  <p>
+                    <span className="font-semibold">Services:</span>{" "}
+                    {selectedEvent.transactionsData?.appointment?.service
+                      .map((s) => s?.service_name)
+                      .join(", ")}
+                  </p>
+                  <p>
+                    <span className="font-semibold">Beautician:</span>{" "}
+                    {
+                      selectedEvent.transactionsData?.appointment?.beautician
+                        ?.name
+                    }
+                  </p>
+                </div>
+                <div className="mt-4 text-xl">
+                  <p className="font-semibold">
+                    Start Time: {moment(selectedEvent.start).format("hh:mm A")}
+                  </p>
+                  <p className="font-semibold">
+                    End Time: {moment(selectedEvent.end).format("hh:mm A")}
+                  </p>
+                </div>
+                <h1 className="pt-4 text-lg font-semibold text-center">
+                  Copy Of Customer's Receipt
+                </h1>
+                <div className="grid items-center justify-center pt-2">
+                  <img
+                    src={selectedEvent.transactionsData?.qrCode}
+                    alt="qr code"
+                    className="w-48 h-48 rounded-xl"
+                  />
+                </div>
+                <span className="grid items-center justify-center">
+                  <button
+                    onClick={handleCloseModal}
+                    className="w-full px-20 py-2 mt-6 border rounded border-primary-accent text-dark-default dark:text-light-default hover:bg-primary-default focus:outline-none"
+                  >
+                    Close
+                  </button>
+                </span>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </>
   );
