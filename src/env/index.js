6e9420789bb178786306933746ee0bcfe58bb0ae
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-26 19:00:11 +0800

    Connect real deployed backend

--- src/env/index.js
+++ src/env/index.js
@@ -1,2 +1,3 @@
-export const API_URL = "http://localhost:4000/api/v1/";
+// export const API_URL = "http://localhost:4000/api/v1/";
+export const API_URL = "https://salon-api-b2m0.onrender.com";
 export const WEB_APP = "development";
