61eaac0aa10b97dca93a7a8fc0f6834b84156ecc
Jay200123 <kenken.ayonon@gmail.com>
2024-01-27 20:52:15 +0800

    Change navbar name to Lhanlee Beauty Lounge

--- src/components/Navbar/BeauticianNavbar.jsx
+++ src/components/Navbar/BeauticianNavbar.jsx
@@ -71,7 +71,7 @@ export default function () {
               className="cursor-pointer"
             />
             <button className="text-xl normal-case btn btn-ghost hover:bg-transparent">
-              Lhanlee Salon
+            Lhanlee Beauty Lounge
             </button>
           </span>
         </div>
