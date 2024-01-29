d33e75ac5a8c80491ba91e937d493b4a1f6d0d60
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-29 14:51:18 +0800

    Refactor Shift

--- src/pages/Shift/index.jsx
+++ src/pages/Shift/index.jsx
@@ -2,65 +2,62 @@ import React, { useState, useEffect } from "react";
 import { BeauticianSidebar } from "@/components";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
-import { useGetTimesQuery } from "@api";
+import {
+  useGetTimesQuery,
+  useAddScheduleMutation,
+  useGetSchedulesQuery,
+} from "@api";
 import { FadeLoader } from "react-spinners";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useFormik } from "formik";
+import { createExcuseValidation } from "@validation";
 
 export default function () {
   const { data: time, isLoading: timeLoading } = useGetTimesQuery();
-  const today = new Date();
+  const [addSchedule] = useAddScheduleMutation();
+  const { user } = useSelector((state) => state.auth);
+  const { data: allSchedules } = useGetSchedulesQuery();
 
   const getNextDays = () => {
     const days = [];
-    for (let i = 0; i < 7; i++) {
+    for (let i = 0; i < 9; i++) {
       const nextDay = new Date();
-      nextDay.setDate(today.getDate() + i);
+      nextDay.setDate(nextDay.getDate() + i);
+      const formattedDate = nextDay.toISOString().split("T")[0];
       const nextDayOptions = {
         weekday: "long",
         year: "numeric",
         month: "long",
         day: "numeric",
       };
-      days.push({
-        dayOfWeek: nextDay.toLocaleDateString("en-PH", nextDayOptions),
-      });
+
+      const formattedDateWithoutTime = formattedDate.split("T")[0];
+
+      const isLeaveForDate = allSchedules?.details.some(
+        (schedule) =>
+          schedule.beautician._id === user?._id &&
+          schedule.date.split("T")[0] === formattedDateWithoutTime &&
+          schedule.isLeave
+      );
+
+      if (!isLeaveForDate) {
+        days.push({
+          dayOfWeek: nextDay.toLocaleDateString("en-PH", nextDayOptions),
+          date: formattedDate,
+        });
+      }
     }
     return days;
   };
 
-  const isWithinRange = (date) => {
-    const today = new Date();
-    const endOfNextMonth = new Date(
-      today.getFullYear(),
-      today.getMonth() + 2,
-      0
-    );
-
-    return date >= today && date <= endOfNextMonth;
-  };
-
-  const tileDisabled = ({ date }) => {
-    const today = new Date();
-    const endOfNextMonth = new Date(
-      today.getFullYear(),
-      today.getMonth() + 2,
-      0
-    );
-
-    return date < today || date > endOfNextMonth;
-  };
-
-  const handleDateChange = (date) => {
-    const selectedDate = new Date(date);
-    selectedDate.setDate(selectedDate.getDate() + 1);
-
-    const formatted = selectedDate.toISOString().split("T")[0];
-    console.log(formatted);
-  };
-
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedTimes, setSelectedTimes] = useState([]);
+  const [selectedDate, setSelectedDate] = useState(new Date());
 
-  const openModal = () => {
+  const openModal = (date) => {
+    setSelectedDate(date);
     setModalOpen(true);
   };
 
@@ -82,13 +79,123 @@ export default function () {
   }, [isModalOpen]);
 
   const handleTimeClick = (time) => {
-    if (selectedTimes.includes(time)) {
-      setSelectedTimes(
-        selectedTimes.filter((selectedTime) => selectedTime !== time)
+    setSelectedTimes((prevTimes) =>
+      prevTimes.includes(time)
+        ? prevTimes.filter((selectedTime) => selectedTime !== time)
+        : [...prevTimes, time]
+    );
+  };
+
+  const formikSchedule = useFormik({
+    initialValues: {
+      beautician: user?._id,
+      date: "",
+      isAvailable: [],
+    },
+    onSubmit: async (values) => {
+      if (values.isAvailable.length < 5) {
+        const toastProps = {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 5000,
+        };
+        toast.warning("Minimum of 5 Shifts per day", toastProps);
+        return;
+      }
+      values.isAvailable = selectedTimes;
+      values.date = selectedDate;
+      // const adjustedDate = new Date(selectedDate);
+      // adjustedDate.setDate(adjustedDate.getDate() + 1);
+      // values.date = adjustedDate.toISOString().split("T")[0];
+
+      addSchedule(values).then((response) => {
+        const toastProps = {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 5000,
+        };
+        if (response?.data?.success === true) {
+          setModalOpen(false);
+          setSelectedTimes([]);
+          toast.success(`${response?.data?.message}`, toastProps);
+        } else
+          toast.error(`${response?.error?.data?.error?.message}`, toastProps);
+      });
+    },
+  });
+
+  const getDefaultDate = () => {
+    const today = new Date();
+    const endOfCurrentWeek = new Date(today);
+    endOfCurrentWeek.setDate(today.getDate() + 7 - today.getDay() + 1);
+
+    return endOfCurrentWeek;
+  };
+
+  const formikExcuse = useFormik({
+    initialValues: {
+      beautician: user?._id,
+      date: getDefaultDate().toISOString().split("T")[0],
+      isLeave: true,
+      leaveNote: "",
+      isAvailable: [],
+    },
+    validationSchema: createExcuseValidation,
+    onSubmit: async (values) => {
+      addSchedule(values).then((response) => {
+        const toastProps = {
+          position: toast.POSITION.TOP_RIGHT,
+          autoClose: 5000,
+        };
+        if (response?.data?.success === true) {
+          formikExcuse.resetForm();
+          toast.success(`${response?.data?.message}`, toastProps);
+        } else
+          toast.error(`${response?.error?.data?.error?.message}`, toastProps);
+      });
+    },
+  });
+
+  const isWithinRange = (date) => {
+    const today = new Date();
+    const next7Days = new Date(today);
+    next7Days.setDate(today.getDate() + 7);
+
+    return date > today && date <= next7Days;
+  };
+
+  const tileDisabled = ({ date }) => {
+    const today = new Date();
+    const endOfCurrentWeek = new Date(today);
+    endOfCurrentWeek.setDate(today.getDate() + 7 - today.getDay());
+
+    return date <= endOfCurrentWeek;
+  };
+
+  const handleDateChange = (date) => {
+    const selectedDate = new Date(date);
+    selectedDate.setDate(selectedDate.getDate() + 1);
+
+    const formatted = selectedDate.toISOString().split("T")[0];
+    formikExcuse.setFieldValue("date", formatted);
+  };
+
+  const getAvailableShiftsCount = (date) => {
+    const matchingSchedules = allSchedules?.details.filter(
+      (schedule) =>
+        schedule.beautician._id === user?._id &&
+        schedule.date.split("T")[0] === date &&
+        !schedule.isLeave
+    );
+
+    if (matchingSchedules) {
+      const totalShifts = matchingSchedules.reduce(
+        (count, schedule) => count + schedule.isAvailable.length,
+        0
       );
-    } else {
-      setSelectedTimes([...selectedTimes, time]);
+
+      return totalShifts;
     }
+
+    return 0;
   };
 
   return (
@@ -116,11 +223,17 @@ export default function () {
                         <h1 className="text-base font-semibold">
                           {day.dayOfWeek}
                         </h1>
-                        <h3>5 hours</h3>
+                        <h3>
+                          {getAvailableShiftsCount(day.date) === 0 ? (
+                            <p>No Shift</p>
+                          ) : (
+                            <h3>{getAvailableShiftsCount(day.date)} hours</h3>
+                          )}
+                        </h3>
                       </div>
                       <div
                         type="button"
-                        onClick={openModal}
+                        onClick={() => openModal(day.date)}
                         className="cursor-pointer grid items-center justify-center my-[.35rem] bg-secondary-variant rounded-xl md:h-full 2xl:h-3/4 w-full font-semibold text-dark-default dark:text-light-default"
                       >
                         <button>Add Shift</button>
@@ -133,76 +246,96 @@ export default function () {
                 <h1 className="pb-6 text-2xl font-semibold text-dark-default dark:text-light-default">
                   File Leave
                 </h1>
-                <div className="grid grid-flow-row-dense gap-y-6">
-                  <Calendar
-                    onChange={handleDateChange}
-                    tileDisabled={tileDisabled}
-                    className={`w-fit text-center border-dark-default dark:border-light-default border-2 !important`}
-                    tileClassName={({ date }) =>
-                      isWithinRange(date)
-                        ? "cursor-pointer hover:bg-primary-accent focus:bg-primary-accent active:bg-primary-accent !important"
-                        : "bg-primary-default !important"
-                    }
-                  />
-                  <div className="text-2xl font-semibold text-dark-default dark:text-light-default">
-                    Leave Note
+                <form onSubmit={formikExcuse.handleSubmit}>
+                  <div className="grid grid-flow-row-dense gap-y-6">
+                    <Calendar
+                      onChange={handleDateChange}
+                      value={formikExcuse.values.date}
+                      tileDisabled={tileDisabled}
+                      className={`w-fit text-center border-dark-default dark:border-light-default border-2 !important`}
+                      tileClassName={({ date }) =>
+                        isWithinRange(date)
+                          ? "cursor-pointer hover:bg-primary-accent focus:bg-primary-accent active:bg-primary-accent !important"
+                          : "bg-primary-default !important"
+                      }
+                    />
+                    <div className="text-2xl font-semibold text-dark-default dark:text-light-default">
+                      Leave Note
+                    </div>
+                    <textarea
+                      id="leaveNote"
+                      name="leaveNote"
+                      autoComplete="off"
+                      placeholder="Leave a note about your reason for leave here..."
+                      className="resize-none block my-4 xl:text-xl lg:text-[1rem] md:text-sm placeholder-dark-default dark:placeholder-light-default border-2 bg-card-input w-full border-dark-default dark:border-light-default focus:ring-0 focus:border-secondary-default rounded-lg"
+                      rows="6"
+                      onChange={formikExcuse.handleChange}
+                      onBlur={formikExcuse.handleBlur}
+                      value={formikExcuse.values.leaveNote}
+                    ></textarea>
+                    {formikExcuse.errors.leaveNote &&
+                      formikExcuse.touched.leaveNote && (
+                        <div className="text-red-500">
+                          {formikExcuse.errors.leaveNote}
+                        </div>
+                      )}
+                    <button
+                      type="submit"
+                      className="grid items-center justify-center py-2 text-2xl rounded-lg 2xl:mx-48 xl:mx-32 lg:mx-24 md:mx-16 bg-secondary-default"
+                    >
+                      Submit
+                    </button>
                   </div>
-                  <textarea
-                    id="description"
-                    name="description"
-                    autoComplete="off"
-                    placeholder="Leave a note about your reason of leave here..."
-                    className="resize-none block my-4 xl:text-xl lg:text-[1rem] md:text-sm placeholder-dark-default dark:placeholder-light-default border-2 bg-card-input w-full border-dark-default dark:border-light-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 rounded-lg"
-                    rows="6"
-                  ></textarea>
-                  <button className="grid items-center justify-center py-2 text-2xl rounded-lg 2xl:mx-48 xl:mx-32 lg:mx-24 md:mx-16 bg-secondary-default">
-                    Submit
+                </form>
+              </div>
+            </div>
+          </div>
+          {isModalOpen && (
+            <div className="z-[1000] fixed inset-0 flex items-center justify-center bg-opacity-60 bg-neutral-primary">
+              <div className="px-3 py-6 bg-light-default dark:bg-dark-default rounded-xl">
+                <h1 className="text-xl font-semibold text-center">
+                  Add Your Time Shift
+                </h1>
+                <div className="grid grid-flow-row-dense grid-cols-3 gap-6 p-8 rounded-lg ">
+                  {time?.details?.map(({ _id, time }) => (
+                    <div
+                      key={_id}
+                      className={`${
+                        selectedTimes.includes(time)
+                          ? "bg-primary-accent"
+                          : "hover:bg-primary-accent focus:bg-primary-accent active:bg-primary-accent"
+                      } cursor-pointer grid items-center justify-center 2xl:mx-4 xl:mx-3 lg:mx-2 md:mx-1 rounded-xl text-dark-default dark:text-light-default border-dark-default dark:border-light-default border-2 p-3`}
+                      onClick={() => handleTimeClick(time)}
+                    >
+                      <h1 className="xl:text-base md:text-sm">{time}</h1>
+                    </div>
+                  ))}
+                </div>
+                <div className="grid items-center justify-center grid-flow-col-dense mt-4 gap-x-4">
+                  <button
+                    type="submit"
+                    onClick={() => {
+                      formikSchedule.setFieldValue(
+                        "isAvailable",
+                        selectedTimes
+                      );
+                      formikSchedule.submitForm();
+                    }}
+                    className="px-6 py-2 font-semibold rounded-md bg-primary-default"
+                  >
+                    Confirm
+                  </button>
+                  <button
+                    type="button"
+                    onClick={closeModal}
+                    className="px-6 py-2 font-semibold border rounded-md border-primary-default hover:bg-primary-accent hover:text-light-accent dark:hover:bg-primary-accent dark:hover:text-light-accent"
+                  >
+                    Close
                   </button>
                 </div>
               </div>
             </div>
-            {isModalOpen && (
-              <>
-                <div className="fixed inset-0 flex items-center justify-center bg-opacity-60 bg-neutral-primary">
-                  <div className="px-3 py-6 bg-light-default dark:bg-dark-default rounded-xl">
-                    <h1 className="text-xl font-semibold text-center">
-                      Add Your Time Shift
-                    </h1>
-                    <div className="grid grid-flow-row-dense grid-cols-3 gap-6 p-8 rounded-lg ">
-                      {time?.details?.map(({ _id, time }) => (
-                        <div
-                          key={_id}
-                          className={`${
-                            selectedTimes.includes(time)
-                              ? "bg-primary-accent"
-                              : "hover:bg-primary-accent focus:bg-primary-accent active:bg-primary-accent"
-                          } cursor-pointer grid items-center justify-center 2xl:mx-4 xl:mx-3 lg:mx-2 md:mx-1 rounded-xl text-dark-default dark:text-light-default border-dark-default dark:border-light-default border-2 p-3`}
-                          onClick={() => handleTimeClick(time)}
-                        >
-                          <h1 className="xl:text-base md:text-sm">{time}</h1>
-                        </div>
-                      ))}
-                    </div>
-                    <div className="grid items-center justify-center grid-flow-col-dense mt-4 gap-x-4">
-                      <button
-                        type="submit"
-                        className="px-6 py-2 font-semibold rounded-md bg-primary-default"
-                      >
-                        Confirm
-                      </button>
-                      <button
-                        type="button"
-                        onClick={closeModal}
-                        className="px-6 py-2 font-semibold border rounded-md border-primary-default hover:bg-primary-accent hover:text-light-accent dark:hover:bg-primary-accent dark:hover:text-light-accent"
-                      >
-                        Close
-                      </button>
-                    </div>
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
+          )}
         </>
       )}
     </>
