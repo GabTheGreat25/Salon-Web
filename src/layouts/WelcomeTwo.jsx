0eaa45fc4e6d2b7c898b54029bd817d66a035d33
Jay200123 <kenken.ayonon@gmail.com>
2024-02-02 01:27:03 +0800

    Add additional info for beautician hiring slide

--- src/layouts/WelcomeTwo.jsx
+++ src/layouts/WelcomeTwo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Welcome } from "@components";
-import Logo2 from "@assets/Logo-2.png";
+import Logo2 from "@assets/testImg2.png";
 import { selectDate, selectTime, selectIsHiring } from "@hiring";
 import { useSelector } from "react-redux";
 
@@ -10,10 +10,11 @@ export default function () {
   const hiring = useSelector(selectIsHiring);
 
   const defaultTitle = `Become a Lhanlee <br /> Beautician!`;
-  const hiringTitle = `Were Hiring! <br/>Apply now!`;
+  const hiringTitle = `Were Hiring! <br/>Apply now! <br/>`;
 
   const msg = `Are you passionate about beauty and wellness?  <br /> Turn your passion into a rewarding career  <br /> as a beautician at Lhanlee Beauty Lounge..`;
-  const hiringMsg = `Hiring Date: ${date} <br/> Appointed Time Slot ${time}`;
+  const hiringMsg = `Hiring Date: ${date} <br/> Hiring Time Slot ${time} <br/> Bring Your Own Resume with Updated: </br>  contact
+  information, education, and relevant work experience.`;
 
   const title = hiring ? hiringTitle : defaultTitle;
   const info = hiring ? hiringMsg : msg;
@@ -24,7 +25,7 @@ export default function () {
         leftArrow={true}
         navigateLeft="/"
         title={title}
-        description={info}
+        description={hiringMsg}
         buttonTitle="Learn More"
         navigateTo="/ChooseRole"
         logo={Logo2}
