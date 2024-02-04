cdb7774d1375ab7a769c7a14748d1ed56a077932
Jay200123 <kenken.ayonon@gmail.com>
2024-02-05 00:35:13 +0800

    Change Lorem Ipsum

--- src/pages/BrandTable/createBrand.jsx
+++ src/pages/BrandTable/createBrand.jsx
@@ -48,8 +48,7 @@ export default function () {
                   Create Brand
                 </h1>
                 <p className="text-xl text-center lg:px-12 text-light-default dark:text-dark-default">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                  Excepturi, laborum!
+                  Create a New Product Brand in Lhanlee Beauty Lounge 
                 </p>
               </span>
               <div className="overflow-x-hidden grid grid-cols-[50%_50%] items-start justify-start pt-20 pb-6 gap-x-6 2xl:pr-0 md:pr-10">
