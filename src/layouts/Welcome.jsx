c28f4fa2d5b583327e1a70b820917048c396c441
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-06 17:47:04 +0800

    Fix case sensitive for deployment

--- src/layouts/Welcome.jsx
+++ src/layouts/Welcome.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Welcome } from "@components";
-import Logo1 from "@assets/Logo-light.png";
+import Logo1 from "@assets/Logo-Light.png";
 
 export default function () {
   return (
