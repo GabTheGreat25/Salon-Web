e090d8fd846984c6f4f95863c415a2821d6e24d5
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-10 01:33:29 +0800

    Refactor editAbsent validation

--- src/validation/editAbsence.js
+++ src/validation/editAbsence.js
@@ -1,11 +1,6 @@
 import * as yup from "yup";
 
 export default yup.object({
-    status: yup
-    .string()
-    .required("Status Required"),
-    leaveNote: yup.
-    string()
-    .required("Leave Note Required"),
-})
-
+  status: yup.string().required("Status Required"),
+  leaveNote: yup.string(),
+});
