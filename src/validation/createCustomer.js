3fd4460e1bc77124b0f7dc336561bf33c4af8214
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-10 01:33:49 +0800

    Add longer validation for description in createCustomer

--- src/validation/createCustomer.js
+++ src/validation/createCustomer.js
@@ -33,10 +33,16 @@ export default yup.object({
     .string("Enter your confirm password")
     .oneOf([yup.ref("password"), null], "Passwords must match")
     .required("Confirm password is required"),
-    description: yup.string()
+  description: yup
+    .string()
     .required("Description is required")
-    .test("custom-validation", "Description must contain terms like 'Long Hair', 'Short Hair', 'Tall Guy', etc.", function (value) {
-      const pattern = /long\s+hair|short\s+hair|tall\s+guy/i;
-      return pattern.test(value);
-    }),
+    .test(
+      "custom-validation",
+      "Please ensure your description includes essential details such as hair length (e.g., 'Long Hair', 'Short Hair'), eye and hair colors (e.g., black, white), and gender ('Male' or 'Female') to facilitate accurate service matching. Thank you for your cooperation.",
+      function (value) {
+        const pattern =
+          /long\s+hair|short\s+hair|black|white|red|pink|blue|green|yellow|orange|purple|brown|gray|grey|beige|cyan|maroon|navy|olive|teal|violet|indigo|turquoise|lavender|magenta|coral|crimson|gold|silver|plum|tan|salmon|ivory|peach|lime|ruby|sapphire|emerald|taupe|chartreuse|periwinkle|mauve|amber|fuchsia|orchid|sepia|cobalt|topaz|jade|cerulean|rust|khaki|umber|saffron|ochre|rose|vermilion|buff|copper|cream|indigo|auburn|chestnut|strawberry\s+blonde|ginger|platinum\s+blonde|dirty\s+blonde|light\s+brown|dark\s+brown|black\s+brown|silver\s+blonde|ash\s+blonde|ash\s+brown|ash\s+blond|ash\s+brunette|chocolate\s+brown|caramel\s+brown|bronze\s+brown|bronze\s+blonde|copper\s+red|fiery\s+red|russet\s+red|mahogany\s+brown|mahogany\s+red|burgundy\s+red|auburn\s+brown|auburn\s+red|auburn\s+blonde|strawberry\s+blonde|red\s+blonde|red\s+brown|blonde\s+red|brunette\s+red|brown\s+red|auburn\s+blonde|auburn\s+brown|auburn\s+brunette|auburn\s+blond|auburn\s+red|auburn\s+copper|blonde\s+brown|blond\s+brown|blond\s+brunette|blond\s+brunet|blond\s+blonde|brown\s+blond|brown\s+brunette|brown\s+brunet|brunet\s+blonde|brunet\s+brown|brunet\s+brunette|auburn\s+blonde|auburn\s+brown|auburn\s+brunette|auburn\s+blond|auburn\s+red|auburn\s+copper|auburn\s+orange/i;
+        return pattern.test(value);
+      }
+    ),
 });
