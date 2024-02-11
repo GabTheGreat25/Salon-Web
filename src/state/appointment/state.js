27310e327f1522fff4ad8c1b53f6fe83400ae04b
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-11 13:20:50 +0800

    Add options in initial state

--- src/state/appointment/state.js
+++ src/state/appointment/state.js
@@ -3,13 +3,20 @@ export const initialState = {
     {
       service_id: "",
       service_name: "",
+      options: [
+        {
+          option_id: "",
+          option_name: "",
+          per_price: 0,
+          extraFee: 0,
+        },
+      ],
       type: [],
       duration: 0,
       description: "",
       product_name: "",
       image: [],
       price: 0,
-      extraFee: 0,
     },
   ],
 };
