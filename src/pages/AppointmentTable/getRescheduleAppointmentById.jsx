cf80296dedb59b27c8919f8b1edf38056477f0c9
Jay200123 <kenken.ayonon@gmail.com>
2024-02-18 22:57:54 +0800

    Add reschedule appointment view page

---
+++ src/pages/AppointmentTable/getRescheduleAppointmentById.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { Card, CardImage } from "@components";
+import { FadeLoader } from "react-spinners";
+import { useGetRescheduleAppointmentByIdQuery } from "@api";
+import { useParams } from "react-router-dom";
+
+export default function () {
+  const { id } = useParams();
+  const { data, isLoading } = useGetRescheduleAppointmentByIdQuery(id);
+  const appointment = data?.details;
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="loader">
+          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+        </div>
+      ) : (
+        <>
+          <Card>
+            <div className="grid w-full h-full pb-10 text-light-default dark:text-dark-default">
+              <span className="grid items-end justify-center">
+                <h1 className="pt-10 font-semibold lg:text-5xl md:text-4xl">
+                  Reschedule Appointment Information
+                </h1>
+              </span>
+              <div className="grid grid-cols-[40%_60%] items-start justify-start pt-6 gap-x-6">
+                <span className="grid items-end justify-end h-[90%] mt-20">
+                  <CardImage />
+                </span>
+                <div className="grid grid-flow-row-dense pr-10 gap-y-4">
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      Customer Name:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={appointment?.customer?.name}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      Appointment Day:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={
+                        new Date(appointment?.date).toISOString().split("T")[0]
+                      }
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      Customer Rebook Reason:
+                    </span>
+                    <textarea
+                      value={appointment?.messageReason}
+                      readOnly
+                      className="resize-none block my-4 xl:text-xl lg:text-[1rem] md:text-sm placeholder-white border-2 bg-card-input w-full border-light-default dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default rounded-lg"
+                    ></textarea>
+                  </label>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </>
+      )}
+    </>
+  );
+}
