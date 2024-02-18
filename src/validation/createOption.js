82f8d479a49adcfc4d019172791f95e247777138
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-18 14:01:20 +0800

    update createOption validation

--- src/validation/createOption.js
+++ src/validation/createOption.js
@@ -1,10 +1,10 @@
 import * as yup from "yup";
 
 export default yup.object({
-    option_name: yup.string().required("Adds On name required"),
-    description: yup.string().required("Description required"),
-    extraFee: yup.string().required("extraFee required"),
-    service: yup
+  option_name: yup.string().required("Adds On name required"),
+  description: yup.string().required("Description required"),
+  extraFee: yup.string().required("extraFee required"),
+  service: yup
     .array()
     .of(yup.string().required("Service  required"))
     .required("At least one service is required"),
