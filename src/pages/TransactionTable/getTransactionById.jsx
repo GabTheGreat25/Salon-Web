ed3f48e0cdc64a06dc38a8d1854137e83cdf1860
Jay200123 <kenken.ayonon@gmail.com>
2024-02-24 15:26:58 +0800

    Add view transaction by id page

---
+++ src/pages/TransactionTable/getTransactionById.jsx
@@ -0,0 +1,127 @@
+import { React } from "react";
+import { useParams } from "react-router-dom";
+import { useGetTransactionByIdQuery } from "@api";
+import { FadeLoader } from "react-spinners";
+import { Card, CardImage } from "@components";
+
+export default function () {
+    const { id } = useParams();
+
+    const { data, isLoading } = useGetTransactionByIdQuery(id);
+    const transaction = data?.details;
+
+    const randomImage =
+    transaction?.image?.length > 0
+      ? transaction?.image[Math.floor(Math.random() * transaction?.image?.length)]
+      : null;
+   
+    return (
+        <>
+          {isLoading ? (
+            <div className="loader">
+              <FadeLoader color="#FDA7DF" loading={true} size={50} />
+            </div>
+          ) : (
+            <>
+              <Card>
+                <div className="grid w-full h-full pb-10 text-light-default dark:text-dark-default">
+                  <span className="grid items-end justify-center">
+                    <h1 className="pt-10 font-semibold lg:text-5xl md:text-4xl">
+                      Transaction Details
+                    </h1>
+                  </span>
+                  <div className="grid grid-cols-[40%_60%] items-start justify-start pt-6 gap-x-6">
+                    <span className="grid items-end justify-end h-[90%] mt-20">
+                      <CardImage />
+                    </span>
+                    <div className="grid grid-flow-row-dense pr-10 gap-y-4">
+                      <label className="block">
+                        <div className="flex items-center justify-center">
+                          {randomImage && (
+                            <img
+                              src={randomImage.url}
+                              alt={randomImage.originalname}
+                              key={randomImage._id}
+                              className="rounded-full"
+                            />
+                          )}
+                        </div>
+                      </label>
+                      <label className="block">
+                        <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                         Assigned Beauticians:
+                        </span>
+                        <input
+                          type="text"
+                          readOnly
+                          value={transaction?.appointment?.beautician?.name}
+                          className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        />
+                      </label>
+                      <label className="block">
+                        <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                            Appointment Customer
+                        </span>
+                        <input
+                          type="text"
+                          readOnly
+                          value={transaction?.appointment?.customer?.name}
+                          className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        />
+                      </label>
+                      <label className="block">
+                        <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                          Appointment Date:
+                        </span>
+                        <input
+                          type="text"
+                          readOnly
+                          value={transaction?.appointment?.date}
+                          className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        />
+                      </label>
+                      <label className="block">
+                        <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                          Appointment Time:
+                        </span>
+                        <input
+                          type="text"
+                          readOnly
+                          value={transaction?.appointment?.time}
+                          className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        />
+                      </label>
+                      <label className="block">
+                        <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                          Payment Method:
+                        </span>
+                        <input
+                          type="text"
+                          readOnly
+                          value={transaction?.payment}
+                          className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        />
+                      </label>
+
+                      <label className="block">
+                        <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                          Transaction Status:
+                        </span>
+                        <input
+                          type="text"
+                          readOnly
+                          value={transaction?.status}
+                          className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        />
+                      </label>
+                      
+                      
+                    </div>
+                  </div>
+                </div>
+              </Card>
+            </>
+          )}
+        </>
+      );
+}
