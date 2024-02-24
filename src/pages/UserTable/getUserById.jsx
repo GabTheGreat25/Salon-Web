1795a264dea86fee196a3b4961059db23acde730
Jay200123 <kenken.ayonon@gmail.com>
2024-02-24 15:27:21 +0800

    Add view user by id page

---
+++ src/pages/UserTable/getUserById.jsx
@@ -0,0 +1,114 @@
+import { React } from "react";
+import { useParams } from "react-router-dom";
+import { useGetUserByIdQuery } from "@api";
+import { FadeLoader } from "react-spinners";
+import { Card, CardImage } from "@components";
+
+export default function () {
+  const { id } = useParams();
+
+  const { data, isLoading } = useGetUserByIdQuery(id);
+  const user = data?.details;
+
+  const randomImage =
+    user?.image?.length > 0
+      ? user?.image[Math.floor(Math.random() * user?.image?.length)]
+      : null;
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="loader">
+          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+        </div>
+      ) : (
+        <>
+          <Card>
+            <div className="grid w-full h-full pb-10 text-light-default dark:text-dark-default">
+              <span className="grid items-end justify-center">
+                <h1 className="pt-10 font-semibold lg:text-5xl md:text-4xl">
+                  User Information
+                </h1>
+              </span>
+              <div className="grid grid-cols-[40%_60%] items-start justify-start pt-6 gap-x-6">
+                <span className="grid items-end justify-end h-[90%] mt-20">
+                  <CardImage />
+                </span>
+                <div className="grid grid-flow-row-dense pr-10 gap-y-4">
+                  <label className="block">
+                    <div className="flex items-center justify-center">
+                      {randomImage && (
+                        <img
+                          src={randomImage.url}
+                          alt={randomImage.originalname}
+                          key={randomImage._id}
+                          className="rounded-full"
+                        />
+                      )}
+                    </div>
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      User Name:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={user?.name}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      Contact Number:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={user?.contact_number}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      User Age:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={user?.age}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      Email:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={user?.email}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      Role:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={user?.roles}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  
+                </div>
+              </div>
+            </div>
+          </Card>
+        </>
+      )}
+    </>
+  );
+}
