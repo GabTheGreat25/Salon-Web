772ab4a580a364f7281638d12b43bb22574884f7
Jay200123 <kenken.ayonon@gmail.com>
2024-03-01 00:01:22 +0800

    Add view exclusion by page

---
+++ src/pages/ExclusionTable/viewExclusionById.jsx
@@ -0,0 +1,64 @@
+import { React } from "react";
+import { useParams } from "react-router-dom";
+import { useGetExclusionByIdQuery } from "@api";
+import { FadeLoader } from "react-spinners";
+import { Card, CardImage } from "@components";
+
+export default function () {
+  const { id } = useParams();
+
+  const { data, isLoading } = useGetExclusionByIdQuery(id);
+  const exclusion = data?.details;
+
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="loader">
+          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+        </div>
+      ) : (
+        <>
+          <Card>
+            <div className="grid w-full h-full pb-10 text-light-default dark:text-dark-default">
+              <span className="grid items-end justify-center">
+                <h1 className="pt-10 font-semibold lg:text-5xl md:text-4xl">
+                  View Chemical Information
+                </h1>
+              </span>
+              <div className="grid grid-cols-[40%_60%] items-start justify-start pt-6 gap-x-6">
+                <span className="grid items-end justify-end h-[90%] mt-20">
+                  <CardImage />
+                </span>
+                <div className="grid grid-flow-row-dense pr-10 gap-y-4">
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      Chemical Name:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={exclusion?.ingredient_name}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                        Chemical Category:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={exclusion?.type}
+                      className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </>
+      )}
+    </>
+  );
+}
