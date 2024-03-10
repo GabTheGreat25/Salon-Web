0437a76975dfcf83ca27cacf275c063266f9d92c
yslaa <crislhan.fabila@tup.edu.ph>
2024-03-10 15:18:23 +0800

    Change fade loader color createoption

--- src/pages/OptionsTable/createOption.jsx
+++ src/pages/OptionsTable/createOption.jsx
@@ -53,7 +53,7 @@ export default function () {
     <>
       {!isLoading || serviceLoading ? (
         <div className="loader">
-          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+          <FadeLoader color="#FFB6C1" loading={true} size={50} />
         </div>
       ) : (
         <>
