63a338f7b22d48121e39c5c2b1c7feb960b29f9d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-10 21:13:41 +0800

    Refactor FeedbackSection component to improve code readability and fix conditional rendering bug

--- src/pages/FeedbackSection/index.jsx
+++ src/pages/FeedbackSection/index.jsx
@@ -1,4 +1,4 @@
-import { React, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useGetFeedbacksQuery } from "@api";
 import { FadeLoader } from "react-spinners";
 import InfiniteScroll from "react-infinite-scroll-component";
@@ -11,7 +11,7 @@ export default function () {
   const [hasMore, setHasMore] = useState(false);
 
   useEffect(() => {
-    if (feedback && feedback) {
+    if (feedback) {
       setItems(feedback.slice(0, 5));
       setHasMore(feedback?.length > 5);
     }
@@ -40,44 +40,44 @@ export default function () {
           next={fetchMoreFeedbacks}
           hasMore={hasMore}
           loader={<h3>Loading...</h3>}
-          endMessage={
-            items?.length === 0 ? (
-              <p className="text-4xl mb-12 text-red-500 font-semibold text-center">
-                No Feedbacks Yet
-              </p>
-            ) : (
-              <p className="text-4xl mb-12 text-red-500 font-semibold text-center">
-                No more feedbacks
-              </p>
-            )
-          }
         >
           <div className="w-full min-h-screen py-6 rounded-lg bg-light-default dark:bg-dark-default">
             <div className="px-8">
-              <h1 className="pb-4 font-semibold xl:text-3xl lg:text-xl md:text-lg">
+              <h1 className="pb-6 font-semibold xl:text-3xl lg:text-xl md:text-lg">
                 Lhanlee Beauty Lounge Feedback
               </h1>
               <div className="grid grid-flow-row-dense gap-y-8">
-                {feedback?.map((f) => (
+                {items.map((f, index) => (
                   <div
                     key={f?._id}
                     className="px-8 py-4 rounded-md bg-primary-default"
                   >
                     <div className="grid items-center justify-center grid-flow-col-dense w-fit gap-x-6">
                       <div>
-                        <h1 className="pb-2 font-semibold xl:text-lg md:text-base">
+                        <h1 className="pb-2 font-semibold xl:text-2xl md:text-lg">
                           {f.isAnonymous
                             ? f.name.charAt(0) + "*".repeat(f.name?.length - 1)
                             : f.name}
                         </h1>
-
-                        <h1 className="py-4 text-lg text-justify">
+                        <h1 className="py-4 text-justify xl:text-3xl md:text-xl">
                           {f.description}
                         </h1>
                       </div>
                     </div>
                   </div>
                 ))}
+                <div className="absolute inset-0 flex items-center justify-center">
+                  {!hasMore && items?.length === 0 && (
+                    <p className="text-4xl font-bold text-center text-primary-accent">
+                      No feedbacks yet
+                    </p>
+                  )}
+                </div>
+                {!hasMore && items?.length > 0 && (
+                  <p className="mb-10 text-4xl font-bold text-center text-primary-accent">
+                    No more feedbacks
+                  </p>
+                )}
               </div>
             </div>
           </div>
