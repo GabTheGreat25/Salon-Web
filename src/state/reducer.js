a140752d5c535768851c17e563e138ab46e76073
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-10 21:18:04 +0800

    Remove countReducer and add notificationReducer to rootReducer

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -2,7 +2,6 @@ import { combineReducers } from "@reduxjs/toolkit";
 import auth from "./auth/authReducer";
 import appointment from "./appointment/appointmentReducer";
 import hiring from "./hiring/hiringReducer";
-import count from "./editSchedule/countReducer";
 import reason from "./editSchedule/reasonReducer";
 import open from "./open/openReducer";
 import ingredient from "./ingredient/ingredientReducer";
@@ -10,13 +9,13 @@ import location from "./auth/locationReducer";
 import waiver from "./waiver/waiverReducer";
 import fee from "./appointment/hasAppointmentReducer";
 import discount from "./discount/discountReducer";
+import notification from "./notification/notificationReducer";
 import { api } from "./api/reducer";
 
 export const rootReducer = combineReducers({
   auth,
   appointment,
   hiring,
-  count,
   reason,
   open,
   ingredient,
@@ -24,5 +23,6 @@ export const rootReducer = combineReducers({
   waiver,
   fee,
   discount,
+  notification,
   [api.reducerPath]: api.reducer,
 });
