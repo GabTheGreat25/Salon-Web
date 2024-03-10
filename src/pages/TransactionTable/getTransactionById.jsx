001fa7d97ee1db205792a4930ab2093564693e60
yslaa <crislhan.fabila@tup.edu.ph>
2024-03-10 15:46:01 +0800

    Change fade loader color gettransactionbyid

--- src/pages/TransactionTable/getTransactionById.jsx
+++ src/pages/TransactionTable/getTransactionById.jsx
@@ -22,7 +22,7 @@ export default function () {
     <>
       {isLoading ? (
         <div className="loader">
-          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+          <FadeLoader color="#FFB6C1" loading={true} size={50} />
         </div>
       ) : (
         <>
