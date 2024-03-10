e79d9aaee60fe16f85600bc73b82f5ee48d405ff
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-10 21:12:26 +0800

    Fix bug in login functionality

--- src/pages/CustomerInfo/index.jsx
+++ src/pages/CustomerInfo/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useParams } from "react-router-dom";
-import { useGetUserByIdQuery, useGetExclusionsQuery  } from "@api";
+import { useGetUserByIdQuery, useGetExclusionsQuery } from "@api";
 import { FadeLoader } from "react-spinners";
 import { Card } from "@components";
 import noImg from "@assets/no-photo.jpg";
@@ -13,16 +13,32 @@ export default function () {
   const randomImage =
     customer?.image?.length > 0
       ? customer?.image[Math.floor(Math.random() * customer?.image?.length)]
+          ?.url
       : null;
 
-  const { data:allergies, isLoading: exclusionLoading } = useGetExclusionsQuery();
+  const { data: allergies, isLoading: exclusionLoading } =
+    useGetExclusionsQuery();
   const exclusions = allergies?.details;
 
-  const filteredExclusions = exclusions
-  ?.filter((exclusion) => customer?.information?.allergy?.includes(exclusion._id))
-  .map((exclusion) => exclusion.ingredient_name);
+  let filteredExclusions = exclusions
+    ?.filter((exclusion) =>
+      customer?.information?.allergy?.includes(exclusion._id)
+    )
+    .map((exclusion) => exclusion.ingredient_name);
 
-  console.log(filteredExclusions);
+  if (
+    Array.isArray(customer?.information?.allergy) &&
+    (customer?.information?.allergy.includes("None") ||
+      customer?.information?.allergy.includes("Others"))
+  ) {
+    filteredExclusions = customer?.information?.allergy;
+  }
+
+  const othersMessage =
+    Array.isArray(customer?.information?.allergy) &&
+    customer?.information?.allergy.includes("Others")
+      ? customer?.information?.othersMessage
+      : null;
 
   return (
     <>
@@ -33,8 +49,8 @@ export default function () {
       ) : (
         <>
           <Card>
-            <div className="grid w-full h-full text-light-default dark:text-dark-default">
-              <span className="grid items-end md:gap-y-10 justify-center 2xl:grid-rows-[90%_10%] xl:grid-rows-[80%_20%] md:grid-rows-[75%_25%]">
+            <div className="grid w-full min-h-screen text-light-default dark:text-dark-default">
+              <span className="grid items-end justify-center">
                 <h1 className="font-semibold text-center xl:text-3xl lg:text-2xl md:text-xl">
                   Customer Information
                 </h1>
@@ -43,47 +59,47 @@ export default function () {
                 <div className="grid grid-cols-[50%_50%] items-center justify-center gap-x-6 2xl:pr-0 md:pr-10 pt-6">
                   <div className="grid items-end justify-center w-full grid-flow-row-dense pr-12 ml-4 gap-y-4">
                     <label className="block">
-                      <span className="text-light-default xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      <span className="text-light-default xl:text-xl md:text-[1rem] font-semibold">
                         Customer Name:
                       </span>
                       <input
                         type="text"
                         readOnly
                         value={customer?.name}
-                        className="block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
                       />
                     </label>
                     <label className="block">
-                      <span className="text-light-default xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      <span className="text-light-default xl:text-xl md:text-[1rem] font-semibold">
                         Customer Email:
                       </span>
                       <input
                         type="text"
                         readOnly
                         value={customer?.email}
-                        className=" block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        className=" block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
                       />
                     </label>
                     <label className="block">
-                      <span className="text-light-default xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      <span className="text-light-default xl:text-xl md:text-[1rem] font-semibold">
                         Mobile Number:
                       </span>
                       <input
                         type="text"
                         readOnly
                         value={customer?.contact_number}
-                        className=" block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        className=" block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
                       />
                     </label>
                     <label className="block">
-                      <span className="text-light-default xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                      <span className="text-light-default xl:text-xl md:text-[1rem] font-semibold">
                         Customer Age:
                       </span>
                       <input
                         type="text"
                         readOnly
                         value={customer?.age}
-                        className=" block mb-2 ml-6 xl:text-lg lg:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                        className=" block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
                       />
                     </label>
                   </div>
@@ -97,20 +113,31 @@ export default function () {
                     </div>
                     <div className="grid items-center justify-between mt-4">
                       <label className="block">
-                        <span className="text-light-default xl:text-xl lg:text-[1rem] md:text-xs font-semibold">
+                        <span className="text-light-default xl:text-xl md:text-[1rem] font-semibold">
                           Chemical Solution Exclusion:
                         </span>
                         <span className="grid grid-cols-2 py-2">
-                            <input
-                             
-                              type="text"
-                              readOnly
-                              value={filteredExclusions} // Convert object to JSON string
-                              className="block mb-2 ml-6 xl:text-lg lg:text-[1rem] border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
-                            />
-                         
+                          <input
+                            type="text"
+                            readOnly
+                            value={filteredExclusions}
+                            className="block mb-2 ml-6 xl:text-lg md:text-[1rem] border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                          />
                         </span>
                       </label>
+                      {othersMessage && (
+                        <label className="block">
+                          <span className="text-light-default xl:text-xl md:text-[1rem] font-semibold">
+                            Type:
+                          </span>
+                          <input
+                            type="text"
+                            readOnly
+                            value={othersMessage}
+                            className="block mb-2 ml-6 xl:text-lg md:text-[1rem] border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                          />
+                        </label>
+                      )}
                     </div>
                   </div>
                 </div>
