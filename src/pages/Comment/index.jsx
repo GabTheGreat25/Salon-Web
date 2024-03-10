82a5d9b0303f870ef0011d748d2132b39c7cacd4
yslaa <crislhan.fabila@tup.edu.ph>
2024-03-10 14:43:20 +0800

    Change fade loader color comment

--- src/pages/Comment/index.jsx
+++ src/pages/Comment/index.jsx
@@ -56,7 +56,7 @@ export default function () {
     <>
       {isLoading || isDeleting ? (
         <div className="loader">
-          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+          <FadeLoader color="#FFB6C1" loading={true} size={50} />
         </div>
       ) : (
         <>
