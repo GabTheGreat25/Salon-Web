a7a5bb6e4b7c02851642c44a8cdedb234f285391
yslaa <crislhan.fabila@tup.edu.ph>
2024-03-10 14:38:21 +0800

    Change fade loader color

--- src/pages/AppointmentTable/editAppointment.jsx
+++ src/pages/AppointmentTable/editAppointment.jsx
@@ -94,7 +94,7 @@ export default function () {
     <>
       {isLoading || servicesLoading ? (
         <div className="loader">
-          <FadeLoader color="#FDA7DF" loading={true} size={50} />
+          <FadeLoader color="#FFB6C1" loading={true} size={50} />
         </div>
       ) : (
         <>
