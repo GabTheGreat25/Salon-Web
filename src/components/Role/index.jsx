578097db05a80f8554fe1f69d5692e42cd45b73d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-10 21:09:55 +0800

    Update Role component styling and text sizes

--- src/components/Role/index.jsx
+++ src/components/Role/index.jsx
@@ -22,17 +22,17 @@ export default function ({
               className="object-contain rounded-tr-lg 2xl:h-[20rem] xl:h-[15rem] lg:h-[10rem] md:h-[8rem] w-[30rem]"
             />
           </span>
-          <div className="items-center px-3 pt-6 pb-3 rounded-b-lg text-dark-default bg-light-default dark:bg-dark-default h-[14rem]">
-            <h1 className="pb-3 font-semibold text-center dark:text-light-default xl:text-base lg:text-sm md:text-xs">
+          <div className="items-center px-3 pt-6 pb-3 rounded-b-lg text-dark-default bg-light-default dark:bg-dark-default h-[18rem]">
+            <h1 className="pb-3 font-semibold text-center dark:text-light-default xl:text-lg lg:text-base md:text-sm">
               {title}
             </h1>
-            <p className="xl:text-xs lg:text-[0.7rem] md:text-[0.65rem] text-center text-neutral-secondary dark:text-light-default">
+            <p className="text-center 2xl:text-lg xl:text-base md:text-sm text-neutral-secondary dark:text-light-default">
               {description}
             </p>
             <span className="grid items-center justify-center py-6">
               <button
                 onClick={() => navigate(navigateTo)}
-                className="xl:px-6 lg:px-4 md:px-2 font-medium capitalize rounded-lg 2xl:text-[.95rem] md:text-xs  btn btn-primary text-light-default dark:text-dark-default"
+                className="font-medium capitalize rounded-lg xl:text-lg md:text-base xl:px-6 lg:px-4 md:px-2 btn btn-primary text-light-default dark:text-dark-default"
               >
                 {buttonTitle}
               </button>
