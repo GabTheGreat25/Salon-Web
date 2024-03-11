9dcd023770d184bacab12d30f8bb068876604b17
Jay200123 <kenken.ayonon@gmail.com>
2024-03-11 20:37:24 +0800

    Add receptionist page components

---
+++ src/components/Footer/ReceptionistFooter.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import {
+  FaGooglePlay,
+  FaPhoneAlt,
+  FaRegEnvelope,
+  FaFacebookSquare,
+  FaCopyright,
+  FaHome,
+  FaUser,
+  FaFileAlt,
+  FaShieldAlt,
+} from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+
+export default function () {
+  const navigate = useNavigate();
+
+  const home = () => {
+    navigate("/receptionist");
+  };
+
+  const profile = () => {
+    navigate("/receptionist/editAdminProfile");
+  };
+
+  const termsCondition = () => {
+    navigate("");
+  };
+
+  const privacyPolicy = () => {
+    navigate("");
+  };
+
+  return (
+    <>
+      <footer className="w-full h-full px-10 pt-5 pb-20 bg-neutral-primary text-light-default dark:bg-light-default dark:text-dark-default">
+        <div className="grid w-full grid-flow-col-dense justify-evenly">
+          <div className="grid justify-center grid-flow-row-dense">
+            <span className="lg:pb-4 md:pb-[.65rem] font-semibold xl:text-base lg:text-[.75rem] md:text-[.6rem] text-primary-default dark:text-dark-default">
+              IMPORTANT INFORMATION
+            </span>
+            <a
+              className="grid grid-flow-col-dense gap-x-2 w-fit items-center pb-2 no-underline link hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]"
+              onClick={home}
+            >
+              <FaHome />
+              <span className="relative top-[1px]">HOME</span>
+            </a>
+            <a
+              className="grid grid-flow-col-dense gap-x-2 w-fit items-center py-2 no-underline link hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]"
+              onClick={profile}
+            >
+              <FaUser />
+              <span className="relative top-[1px]">PROFILE</span>
+            </a>
+            <a
+              className="grid grid-flow-col-dense gap-x-2 w-fit items-center py-2 no-underline link hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]"
+              onClick={termsCondition}
+            >
+              <FaFileAlt />
+              <span className="relative top-[1px]">TERMS & CONDITIONS</span>
+            </a>
+            <a
+              className="grid grid-flow-col-dense gap-x-2 w-fit items-center py-2 no-underline link hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]"
+              onClick={privacyPolicy}
+            >
+              <FaShieldAlt />
+              <span className="relative top-[1px]">PRIVACY POLICY</span>
+            </a>
+          </div>
+          <div className="grid justify-center grid-flow-row-dense h-fit">
+            <span className="font-semibold xl:text-base lg:text-[.75rem] md:text-[.6rem] text-primary-default dark:text-dark-default lg:pb-4 md:pb-[.65rem]">
+              DOWNLOAD
+            </span>
+            <div className="grid grid-flow-col-dense gap-x-2 w-fit items-center hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]">
+              <FaGooglePlay />
+              <a className="no-underline link "> GOOGLE PLAY (ANDROID)</a>
+            </div>
+          </div>
+          <div className="grid justify-center grid-flow-row-dense">
+            <span className="pb-3 font-semibold xl:text-base lg:text-[.75rem] md:text-[.6rem] text-primary-default dark:text-dark-default">
+              CONTACT US
+            </span>
+            <div className="relative grid items-center justify-start grid-flow-col-dense gap-2 bottom-[1.6rem] h-fit hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]">
+              <FaPhoneAlt />
+              <a className="no-underline link "> +63956 802 8031</a>
+            </div>
+            <div className="relative grid items-center justify-start grid-flow-col-dense gap-2 bottom-[2.4rem] h-fit hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]">
+              <FaRegEnvelope />
+              <a className="no-underline link "> alexjijopea@gmail.com</a>
+            </div>
+          </div>
+          <div className="grid justify-center grid-flow-row-dense h-fit">
+            <span className="font-semibold xl:text-base lg:text-[.75rem] md:text-[.6rem] text-primary-default dark:text-dark-default lg:pb-4 md:pb-[.65rem]">
+              CONNECT
+            </span>
+            <div className="grid grid-flow-col-dense gap-x-2 w-fit items-center hover:text-primary-default hover:dark:text-neutral-secondary xl:text-base lg:text-[.75rem] md:text-[.6rem]">
+              <FaFacebookSquare />
+              <a
+                className="no-underline link"
+                href="https://www.facebook.com/Lanleebeautylounge"
+              >
+                facebook.com/lhanleesalon
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="grid justify-end py-8 pr-12">
+          <p className="grid items-center justify-start grid-flow-col-dense gap-2 h-fit">
+            Copyright
+            <span>
+              <FaCopyright />
+            </span>
+            2023 - TEAM LHANLEESALON TAGUIG
+          </p>
+        </div>
+      </footer>
+    </>
+  );
+}
---
+++ src/components/Sidebar/ReceptionistSidebar.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faUserAlt } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+
+export default function () {
+  const user = useSelector((state) => state.auth.user);
+  const navigate = useNavigate();
+
+  const isActiveRoute = (route) =>
+    window.location.pathname.includes(`/receptionist/${route}`);
+
+  const handleGoBack = () => {
+    navigate("/receptionist");
+  };
+
+  return (
+    <>
+      <div className="min-h-screen p-4 mt-2 rounded shadow-lg w-72">
+        <div className="grid items-center justify-center pt-5">
+          <div className="p-4 overflow-hidden font-semibold capitalize xl:text-lg lg:text-base md:text-sm whitespace-nowrap">
+            Welcome Back,
+            <span className="ml-2">
+              {" "}
+              {user?.name?.length > 10
+                ? `${user.name.slice(0, 10)}...`
+                : user.name}
+            </span>
+          </div>
+        </div>
+        <hr className="my-4 border-t border-dark-default dark:border-light-default" />
+        <div className="grid items-center justify-center grid-flow-row-dense">
+          <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
+            <div className="grid justify-center">
+              <FontAwesomeIcon icon={faUserAlt} className="text-lg" />
+            </div>
+            <div className="grid justify-center">
+              <h1 className={`text-lg font-semibold`}>My Account</h1>
+            </div>
+          </div>
+          <NavLink
+            to="/receptionist/editReceptionistProfile"
+            className={`pt-2 text-center cursor-pointer font-base ${
+              isActiveRoute("editBeauticianProfile")
+                ? "text-primary-default"
+                : "text-dark-default dark:text-light-default"
+            }`}
+          >
+            Edit Profile
+          </NavLink>
+          <NavLink
+            to="/receptionist/changePassword"
+            className={`pt-2 pl-6 text-center font-base ${
+              isActiveRoute("changePassword")
+                ? "text-primary-default"
+                : "text-dark-default dark:text-light-default"
+            }`}
+          >
+            Edit Password
+          </NavLink>
+          <NavLink
+            to="/receptionist/leave"
+            className={`pt-2 ml-[-.3rem] text-center font-base ${
+              isActiveRoute("leave")
+                ? "text-primary-default"
+                : "text-dark-default dark:text-light-default"
+            }`}
+          >
+            File Leave
+          </NavLink>
+          <NavLink
+            to="/receptionist/editShift"
+            className={`pt-2 pb-3 ml-[-.2rem] text-center font-base ${
+              isActiveRoute("editShift")
+                ? "text-primary-default"
+                : "text-dark-default dark:text-light-default"
+            }`}
+          >
+            Edit Leave
+          </NavLink>
+          <div className="grid items-center grid-cols-[10%_auto] gap-x-3">
+            <div className="grid justify-center">
+              <FontAwesomeIcon icon={faArrowLeft} className="text-lg" />
+            </div>
+            <div className="grid justify-start">
+              <a
+                onClick={handleGoBack}
+                className={`text-lg font-light cursor-pointer`}
+              >
+                Go Back
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
