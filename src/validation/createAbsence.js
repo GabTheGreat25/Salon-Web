9e3711570d3002ddb8067fdf69748d83126048dd
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-13 22:48:15 +0800

    Update validation messages in createAbsence.js

--- src/validation/createAbsence.js
+++ src/validation/createAbsence.js
@@ -1,7 +1,7 @@
 import * as yup from "yup";
 
 export default yup.object({
-    beautician: yup.string().required("Beautician name required"),
-    date: yup.string().required("Enter a Date"),
-    status: yup.string().required("Status Required")
+  beautician: yup.string().required("Employee name required"),
+  date: yup.string().required("Enter a Date"),
+  status: yup.string().required("Status Required"),
 });
