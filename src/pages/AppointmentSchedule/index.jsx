a3c40b181140be5527303201ee7e619887bd1eb8
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-13 22:36:04 +0800

    Update beautician name to employee name

--- src/pages/AppointmentSchedule/index.jsx
+++ src/pages/AppointmentSchedule/index.jsx
@@ -73,7 +73,7 @@ export default function () {
       sortable: true,
     },
     {
-      name: "Beautician Name",
+      name: "Employee Name",
       selector: (row) =>
         Array.isArray(row.beautician)
           ? row.beautician.map((b) => b.name).join(", ")
