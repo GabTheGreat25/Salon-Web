18b086cc1a72b482bcc9e294a17be27048c7552a
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-14 22:02:31 +0800

    Update WelcomeTwo layout title and hiring message

--- src/layouts/WelcomeTwo.jsx
+++ src/layouts/WelcomeTwo.jsx
@@ -7,19 +7,20 @@ export default function () {
   const { data } = useGetHiringsQuery();
   const hiring = data?.details[0];
 
-  const defaultTitle = `Become a Lhanlee <br /> Beautician!`;
-  const hiringTitle = `Were Hiring A <br/>${hiring.type}<br/> Apply now! <br/>`;
+  const defaultTitle = `Become a Lhanlee <br /> Employee!`;
+  const hiringTitle = `Were Hiring A <br/>${hiring?.type}<br/> Apply now! <br/>`;
 
-  const msg = `Are you passionate about beauty and wellness?  <br /> Turn your passion into a rewarding career  <br /> as a beautician at Lhanlee Beauty Lounge..`;
-  const hiringMsg = `Hiring Date: ${
-    new Date(hiring?.date).toISOString().split("T")[0]
-  } <br/> Hiring Time Slot: ${
-    hiring?.time
-  }  <br/> Bring a Valid ID <br/> Bring Your Own Resume with Updated: </br>  contact
+  const hiringDate = hiring?.date ? new Date(hiring.date) : null;
+  const hiringDateFormatted = hiringDate
+    ? hiringDate.toISOString().split("T")[0]
+    : "";
+
+  const msg = `Are you passionate about beauty and wellness?  <br /> Turn your passion into a rewarding career  <br /> as a employee at Lhanlee Beauty Lounge..`;
+  const hiringMsg = `Hiring Date: ${hiringDateFormatted} <br/> Hiring Time Slot: ${hiring?.time}  <br/> Bring a Valid ID <br/> Bring Your Own Resume with Updated: </br>  contact
   information, education, and relevant work experience.`;
 
-  const title = hiring ? hiringTitle : defaultTitle;
-  const info = hiring ? hiringMsg : msg;
+  const title = hiring?.isHiring === false ? defaultTitle : hiringTitle;
+  const info = hiring?.isHiring === false ? msg : hiringMsg;
 
   return (
     <>
