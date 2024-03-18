6daece5787fa264db2ed5cb3a029bbe4139f0d33
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-18 11:32:55 +0800

    Add loading spinner to IngredientForm component

--- src/pages/Ingredients/Hands.jsx
+++ src/pages/Ingredients/Hands.jsx
@@ -4,6 +4,7 @@ import { ingredientSlice } from "@ingredient";
 import { FaArrowLeft } from "react-icons/fa";
 import { locationSlice } from "@location";
 import { useGetExclusionsQuery } from "@api";
+import { FadeLoader } from "react-spinners";
 
 export default function IngredientForm() {
   const checkedAllergies = useSelector(
@@ -52,43 +53,51 @@ export default function IngredientForm() {
 
   return (
     <>
-      <div className="min-h-screen">
-        <button onClick={goBack} className="px-8 py-4">
-          <FaArrowLeft size={40} />
-        </button>
-        <h1 className="py-6 text-3xl text-center">
-          Choose any of the following <br /> Chemical Solutions that can cause
-          you allergy
-        </h1>
-        <div className="grid grid-flow-row gap-6 pt-2 ml-6 md:grid-cols-5">
-          {filteredAllergies?.map((allergy) => (
-            <div
-              key={allergy?._id}
-              className="grid grid-cols-[20%_80%] justify-center items-center"
-            >
-              <input
-                type="checkbox"
-                id={allergy?.type}
-                value={allergy?.type}
-                checked={
-                  Array.isArray(checkedAllergies) &&
-                  checkedAllergies?.some((item) => item._id === allergy._id)
-                }
-                onChange={() => handleCheckboxChange(allergy)}
-                className="border-2 rounded xl:w-8 xl:h-8 lg:h-6 lg:w-6 md:h-4 md:w-4 border-primary-default focus:border-primary-accent focus:ring-primary-default checked:bg-primary-default checked:dark:bg-dark-default"
-              />
-              <label
-                htmlFor={allergy?.type}
-                className="block cursor-pointer text-xs md:text-sm lg:text-[.9rem] overflow-wrap-break-word"
-                onClick={(e) => handleLabelClick(allergy, e)}
-                style={{ overflowWrap: "break-word" }}
-              >
-                {allergy?.ingredient_name}
-              </label>
-            </div>
-          ))}
+      {isLoading ? (
+        <div className="loader">
+          <FadeLoader color="#FFB6C1" loading={true} size={50} />
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="min-h-screen">
+            <button onClick={goBack} className="px-8 py-4">
+              <FaArrowLeft size={40} />
+            </button>
+            <h1 className="py-6 text-3xl text-center">
+              Choose any of the following <br /> Chemical Solutions that can
+              cause you allergy
+            </h1>
+            <div className="grid grid-flow-row gap-6 pt-2 ml-6 md:grid-cols-5">
+              {filteredAllergies?.map((allergy) => (
+                <div
+                  key={allergy?._id}
+                  className="grid grid-cols-[20%_80%] justify-center items-center"
+                >
+                  <input
+                    type="checkbox"
+                    id={allergy?.type}
+                    value={allergy?.type}
+                    checked={
+                      Array.isArray(checkedAllergies) &&
+                      checkedAllergies?.some((item) => item._id === allergy._id)
+                    }
+                    onChange={() => handleCheckboxChange(allergy)}
+                    className="border-2 rounded xl:w-8 xl:h-8 lg:h-6 lg:w-6 md:h-4 md:w-4 border-primary-default focus:border-primary-accent focus:ring-primary-default checked:bg-primary-default checked:dark:bg-dark-default"
+                  />
+                  <label
+                    htmlFor={allergy?.type}
+                    className="block cursor-pointer text-xs md:text-sm lg:text-[.9rem] overflow-wrap-break-word"
+                    onClick={(e) => handleLabelClick(allergy, e)}
+                    style={{ overflowWrap: "break-word" }}
+                  >
+                    {allergy?.ingredient_name}
+                  </label>
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 }
