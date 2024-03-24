7fa8513b5c4ffd12fe5adc4b3d6ec4d6a9c38fda
Jay200123 <kenken.ayonon@gmail.com>
2024-03-24 22:15:33 +0800

    Add previous volume previous qty and deducted qty columns

--- src/pages/InventoryTable/index.jsx
+++ src/pages/InventoryTable/index.jsx
@@ -15,7 +15,7 @@ export default function () {
   const navigate = useNavigate();
   const { data, isLoading } = useGetInventoriesQuery();
   const inventories = data?.details;
-
+  
   // const [deleteProduct, { isLoading: isDeleting }] = useDeleteProductMutation();
 
   const deletedInventoryIds = getDeletedItemIds("inventories");
@@ -54,9 +54,51 @@ export default function () {
       sortable: true,
     },
     {
+      name: "Service Image",
+      cell: (row) => {
+        const randomImage =
+          row?.service?.image?.length > 0
+            ?  row?.service?.image[Math.floor(Math.random() *  row?.service?.image?.length)]
+            : null;
+
+        return (
+          <div className="grid items-center justify-center">
+            {randomImage && (
+              <img
+                className="object-center w-10 h-10 rounded-full"
+                src={randomImage.url}
+                alt={randomImage.originalname}
+              />
+            )}
+          </div>
+        );
+      },
+    },
+    {
       name: "Service Name",
       selector: (row) => row?.service?.service_name,
       sortable: true,
+    },
+    {
+      name: "Product Images",
+      cell: (row) => {
+        const randomImage =
+          row?.product?.image?.length > 0
+            ?  row?.product?.image[Math.floor(Math.random() *  row?.product?.image?.length)]
+            : null;
+
+        return (
+          <div className="grid items-center justify-center">
+            {randomImage && (
+              <img
+                className="object-center w-10 h-10 rounded-full"
+                src={randomImage.url}
+                alt={randomImage.originalname}
+              />
+            )}
+          </div>
+        );
+      },
     },
     {
       name: "Product Name",
@@ -64,14 +106,34 @@ export default function () {
       sortable: true,
     },
     {
+      name: "Previous Volume",
+      selector: (row) => row?.old_volume >= 1000 ? `${row.old_volume / 1000} Liter` : `${row.old_volume} ml`,
+      sortable: true,
+    },
+    {
       name: "Consumed Amount",
-      selector: (row) => row?.product_consume,
+      selector: (row) => row?.product_consume >= 1000 ? `${row.product_consume / 1000} Liter` : `${row.product_consume} ml`,
+      sortable: true,
+    },
+    {
+      name: "Remaining Volume",
+      selector: (row) => row?.remained_volume >= 1000 ? `${row.remained_volume / 1000} Liter` : `${row.remained_volume} ml`,
+      sortable: true,
+    },
+    {
+      name: "Previous Quantity",
+      selector: (row)=> `${row?.old_quantity} pcs.`,
       sortable: true,
     },
     {
       name:"Remaining Quantity",
-      selector: (row)=> row?.remained_quantity,
-      sortable: true
+      selector: (row)=> `${row?.remained_quantity} pcs.`,
+      sortable: true,
+    },
+    {
+      name:"Deducted Quantity",
+      selector: (row)=> `${row?.deducted_quantity} pcs.`,
+      sortable: true,
     },
     {
       name: "Actions",
@@ -103,7 +165,7 @@ export default function () {
       ) : (
         <div className="min-h-screen m-12 rounded-lg">
           <DataTable
-            title="Product Inventory Table"
+            title="Stocks"
             columns={columns}
             data={filteredInventory}
             pagination
