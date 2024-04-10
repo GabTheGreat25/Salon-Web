5e41eeefa2c8c794f926be2b37dbf1cd8b32ce63
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-04-11 01:55:19 +0800

    Refactor Hiring

--- src/pages/Dashboard/Hiring.jsx
+++ src/pages/Dashboard/Hiring.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Card, CardImage } from "@components";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
