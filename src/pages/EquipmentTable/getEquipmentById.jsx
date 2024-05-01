ecf4ebd752222c672634608ee4ba697dc3cb0177
Jay200123 <kenken.ayonon@gmail.com>
2024-05-02 00:30:24 +0800

    Add getEquipmentById page

---
+++ src/pages/EquipmentTable/getEquipmentById.jsx
@@ -0,0 +1,131 @@
+import { React } from "react";
+import { useParams } from "react-router-dom";
+import { useGetEquipmentByIdQuery } from "@api";
+import { FadeLoader } from "react-spinners";
+import { Card, CardImage } from "@components";
+
+export default function () {
+  const { id } = useParams();
+
+  const { data, isLoading } = useGetEquipmentByIdQuery(id);
+  const equipment = data?.details;
+
+  const randomImage =
+    equipment?.image?.length > 0
+      ? equipment?.image[Math.floor(Math.random() * equipment?.image?.length)]
+      : null;
+
+  return (
+    <>
+      {isLoading ? (
+        <div className="loader">
+          <FadeLoader color="#FFB6C1" loading={true} size={50} />
+        </div>
+      ) : (
+        <>
+          <Card>
+            <div className="grid w-full h-full pb-10 text-light-default dark:text-dark-default">
+              <span className="grid items-end justify-center">
+                <h1 className="pt-10 font-semibold xl:text-5xl md:text-4xl">
+                  Equipment Information
+                </h1>
+              </span>
+              <div className="grid grid-cols-[40%_60%] items-start justify-start pt-6 gap-x-6">
+                <span className="grid items-end justify-end h-[90%] mt-20">
+                  <CardImage />
+                </span>
+                <div className="grid grid-flow-row-dense pr-10 gap-y-4">
+                  <label className="block">
+                    <div className="flex items-center justify-center">
+                      {randomImage && (
+                        <img
+                          src={randomImage.url}
+                          alt={randomImage.originalname}
+                          key={randomImage._id}
+                          className="rounded-lg"
+                        />
+                      )}
+                    </div>
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl md:text-[1rem] font-semibold">
+                      Equipment Name:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={equipment?.equipment_name}
+                      className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl md:text-[1rem] font-semibold">
+                      Equipment Price:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={equipment?.equipment_price}
+                      className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl md:text-[1rem] font-semibold">
+                      Purchased Date:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={
+                        new Date(equipment?.purchased_date).toISOString().split("T")[0]
+                      }
+                      className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl md:text-[1rem] font-semibold">
+                      Equipment Status:
+                    </span>
+                    <input
+                      type="text"
+                      readOnly
+                      value={equipment?.status}
+                      className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl md:text-[1rem] font-semibold">
+                      Equipment Quantity:
+                    </span>
+                    <input
+                      value={equipment?.quantity}
+                      className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl md:text-[1rem] font-semibold">
+                      Loss Quantity:
+                    </span>
+                    <input
+                      value={equipment?.loss_qty}
+                      className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                  <label className="block">
+                    <span className="xl:text-xl md:text-[1rem] font-semibold">
+                      Damage  Quantity:
+                    </span>
+                    <input
+                      value={equipment?.damaged_qty}
+                      className="block mb-2 ml-6 xl:text-lg md:text-[1rem]  border-0 bg-card-input dark:border-dark-default focus:ring-0 focus:border-secondary-t2 focus:dark:focus:border-secondary-t2 dark:placeholder-dark-default w-full"
+                    />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </Card>
+        </>
+      )}
+    </>
+  );
+}
