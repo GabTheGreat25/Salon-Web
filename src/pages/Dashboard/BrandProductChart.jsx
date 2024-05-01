b407f0fb97910fb39b9084d72bb337b41f1c1ba1
Jay200123 <kenken.ayonon@gmail.com>
2024-05-02 00:27:54 +0800

    Add BrandProduct Chart

---
+++ src/pages/Dashboard/BrandProductChart.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import {
+  BarChart,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Bar,
+  ResponsiveContainer,
+} from "recharts";
+import { useGetBrandProductQuery } from "@api";
+import randomColor from "randomcolor";
+
+export default function BrandProductChart() {
+  const { data } = useGetBrandProductQuery();
+
+  const chartData = React.useMemo(() => {
+    if (!data) return [];
+    return data.details.map((item) => ({
+      brandName: item.brandName || "Unknown",
+      productCount: item.productCount || 0,
+      color: randomColor({ luminosity: "bright" }),
+    }));
+  }, [data]);
+
+  const renderCustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload?.length) {
+      const { brandName, productCount } = payload[0].payload;
+
+      return (
+        <div className="text-lg font-bold">
+          <div>{`${productCount} products by ${brandName}`}</div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <ResponsiveContainer height={400}>
+      <h3 className="text-center lg">Brand Product Reports</h3>
+      <BarChart
+        width={600}
+        height={400}
+        data={chartData}
+        margin={{ top: 20, right: 30 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="brandName" />
+        <YAxis />
+        <Tooltip content={renderCustomTooltip} />
+        <Bar dataKey="productCount" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
