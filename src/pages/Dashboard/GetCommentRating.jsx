f4315ce6c55b6598f3d9ce8ce89ed2ca17b0e6ad
Jay200123 <kenken.ayonon@gmail.com>
2024-05-02 00:29:02 +0800

    Add GetCommentRating Chart

---
+++ src/pages/Dashboard/GetCommentRating.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
+import { useGetCommentRatingQuery } from "@api";
+import randomColor from "randomcolor";
+
+export default function() {
+  const { data } = useGetCommentRatingQuery();
+
+  const chartData = React.useMemo(() => {
+    if (!data) return [];
+    return data.details.map((item) => ({
+      name: item._id || "No Rating",
+      value: item.count || 0,
+      color: randomColor({ luminosity: "bright" }),
+    }));
+  }, [data]);
+
+  const COLORS = React.useMemo(() => {
+    return chartData.map((entry) => entry.color);
+  }, [chartData]);
+
+  const renderCustomTooltip = ({ active, payload }) => {
+    if (active && payload && payload?.length) {
+      const { name, value } = payload[0].payload;
+
+      return (
+        <div className="text-lg font-bold">
+          <div>{`${value} ${name}`}</div>
+        </div>
+      );
+    }
+    return null;
+  };
+
+  return (
+    <ResponsiveContainer height={400}>
+      <h3 className="text-center text-lg">Comments</h3>
+      <BarChart data={chartData}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip content={renderCustomTooltip} />
+        <Legend />
+        <Bar dataKey="value" fill="#8884d8" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
