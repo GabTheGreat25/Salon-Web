c6f1d5f41dc54a26665f30aae0ed946d30c3a9a6
Jay200123 <kenken.ayonon@gmail.com>
2024-05-07 22:49:48 +0800

    Change delivery page titles

--- src/pages/DeliveryTable/createDelivery.jsx
+++ src/pages/DeliveryTable/createDelivery.jsx
@@ -118,7 +118,7 @@ export default function () {
             <div className="grid w-full h-full text-light-default dark:text-dark-default">
               <span className="grid items-end md:gap-y-5 2xl:gap-y-10 justify-center 2xl:grid-rows-[90%_10%] xl:grid-rows-[80%_20%] md:grid-rows-[75%_25%]">
                 <h1 className="text-3xl font-semibold text-center">
-                  Create Delivery
+                 Delivery Details
                 </h1>
                 <p className="text-xl text-center lg:px-12 text-light-default dark:text-dark-default">
                   Create a New Delivery At Lhanlee Beauty Lounge
