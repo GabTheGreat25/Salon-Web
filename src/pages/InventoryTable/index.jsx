d74b9c8b38f880238b6aeb6218de9b7e9091b651
Jay200123 <kenken.ayonon@gmail.com>
2024-05-10 21:26:04 +0800

    Refactor LogBook DataTable

--- src/pages/InventoryTable/index.jsx
+++ src/pages/InventoryTable/index.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-import {
- useGetInventoriesQuery
-} from "@api";
+import { React, useEffect } from "react";
+import { useGetInventoriesQuery } from "@api";
 import { FaEye, FaEdit, FaTrash } from "react-icons/fa";
 import { FadeLoader } from "react-spinners";
 import DataTable from "react-data-table-component";
@@ -12,20 +10,53 @@ import { tableCustomStyles } from "../../utils/tableCustomStyles";
 import { useNavigate } from "react-router-dom";
 
 export default function () {
-  const navigate = useNavigate();
-  const { data, isLoading } = useGetInventoriesQuery();
+  const { data, isLoading, refetch } = useGetInventoriesQuery();
   const inventories = data?.details;
-  
+
+  useEffect(() => {
+    const handleFocus = () => {
+      isFocused.current = true;
+      refetch();
+    };
+
+    window.addEventListener("focus", handleFocus);
+
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+    };
+  }, [refetch]);
+
+  // const [deleteProduct, { isLoading: isDeleting }] = useDeleteProductMutation();
+
   const deletedInventoryIds = getDeletedItemIds("inventories");
 
   const filteredInventory = inventories?.filter(
     (inventory) => !deletedInventoryIds?.includes(inventory?._id)
   );
 
+  // const handleDelete = async (id) => {
+  //   if (window.confirm("Are you sure you want to delete this Product?")) {
+  //     const response = await deleteProduct(id);
+
+  //     const toastProps = {
+  //       position: toast.POSITION.TOP_RIGHT,
+  //       autoClose: 3000,
+  //     };
+  //     if (response?.data?.success === true) {
+  //       toast.success(`${response?.data?.message}`, toastProps);
+  //       addDeletedItemId("product", id);
+  //     } else
+  //       toast.error(`${response?.error?.data?.error?.message}`, toastProps);
+  //   }
+  // };
+
   const columns = [
     {
       name: "Appointment Date",
-      selector: (row) => row?.appointment?.date ? new Date(row?.appointment?.date).toISOString().split("T")[0] : "No Appointment",
+      selector: (row) =>
+        row?.appointment?.date
+          ? new Date(row?.appointment?.date).toISOString().split("T")[0]
+          : row?.appointment?.date,
 
       sortable: true,
     },
@@ -39,7 +70,9 @@ export default function () {
       cell: (row) => {
         const randomImage =
           row?.service?.image?.length > 0
-            ?  row?.service?.image[Math.floor(Math.random() *  row?.service?.image?.length)]
+            ? row?.service?.image[
+                Math.floor(Math.random() * row?.service?.image?.length)
+              ]
             : null;
 
         return (
@@ -65,7 +98,9 @@ export default function () {
       cell: (row) => {
         const randomImage =
           row?.product?.image?.length > 0
-            ?  row?.product?.image[Math.floor(Math.random() *  row?.product?.image?.length)]
+            ? row?.product?.image[
+                Math.floor(Math.random() * row?.product?.image?.length)
+              ]
             : null;
 
         return (
@@ -86,35 +121,80 @@ export default function () {
       selector: (row) => row?.product?.product_name,
       sortable: true,
     },
+    // {
+    //   name: "Remaining",
+    //   selector: (row) => {
+    //     const volumeInLiters = row?.remaining_volume >= 1000 ? row.remaining_volume / 1000 : row?.remaining_volume;
+    //     const measurement = row?.product_measurement;
+    //     return `${volumeInLiters} ${measurement}`;
+    // },
+    //   sortable: true,
+    // },
     {
       name: "Previous Volume",
-      selector: (row) => row?.old_volume >= 1000 ? `${row.old_volume / 1000} Liter` : `${row.old_volume} ml`,
+      selector: (row) => {
+        const oldVolume =
+          row?.old_volume >= 1000 ? row.old_volume / 1000 : row?.old_volume;
+        const measurement =
+          row?.product?.volume_description === "Pieces" ? "pcs" : "ml";
+        return `${oldVolume} ${measurement}`;
+      },
       sortable: true,
     },
     {
       name: "Consumed Amount",
-      selector: (row) => row?.product_consume >= 1000 ? `${row.product_consume / 1000} Liter` : `${row.product_consume} ml`,
+      selector: (row) => {
+        const oldVolume = row?.product_consume;
+        const measurement =
+          row?.product?.volume_description === "Pieces" ? "pcs" : "ml";
+        return `${oldVolume} ${measurement}`;
+      },
       sortable: true,
     },
     {
       name: "Remaining Volume",
-      selector: (row) => row?.remained_volume >= 1000 ? `${row.remained_volume / 1000} Liter` : `${row.remained_volume} ml`,
+      selector: (row) =>{
+        const oldVolume = row?.remained_volume;
+        const measurement =
+          row?.product?.volume_description === "Pieces" ? "pcs" : "ml";
+        return `${oldVolume} ${measurement}`;
+      },
       sortable: true,
     },
     {
       name: "Previous Quantity",
-      selector: (row)=> `${row?.old_quantity} pcs.`,
+      selector: (row) => `${row?.old_quantity} pcs.`,
       sortable: true,
     },
     {
-      name:"Deducted Quantity",
-      selector: (row)=> `${row?.deducted_quantity} pcs.`,
+      name: "Deducted Quantity",
+      selector: (row) => `${row?.deducted_quantity} pcs.`,
       sortable: true,
     },
     {
-      name:"Remaining Quantity",
-      selector: (row)=> `${row?.remained_quantity} pcs.`,
+      name: "Remaining Quantity",
+      selector: (row) => `${row?.remained_quantity} pcs.`,
       sortable: true,
+    },
+
+    {
+      name: "Actions",
+      cell: (row) => (
+        <div className="grid grid-flow-col-dense text-center gap-x-4">
+          {/* <FaEye
+          className="text-xl text-green-300"
+          onClick={()=>console.log("view inventory info")}
+          />
+          <FaEdit
+            className="text-xl text-blue-500"
+          onClick={()=>console.log("edit inventory info")}
+          />
+          <FaTrash
+            className="text-xl text-red-500"
+          onClick={()=>console.log("delete inventory info")}
+          /> */}
+        </div>
+      ),
     },
   ];
 
