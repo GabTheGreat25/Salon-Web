ac000ed9be6a74523ab109d65f3398c54328e6e5
Jay200123 <kenken.ayonon@gmail.com>
2024-05-10 21:23:33 +0800

    Refactor GetAppointmentCustomerChart

--- src/pages/Dashboard/GetAppointmentCustomer.jsx
+++ src/pages/Dashboard/GetAppointmentCustomer.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import {
-  PieChart,
-  Pie,
-  Cell,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
   Tooltip,
-  Legend,
   ResponsiveContainer,
+  Legend,
 } from "recharts";
 import { useGetCustomerAppointmentQuery } from "@api";
 import randomColor from "randomcolor";
 
-export default function MyPieChart() {
-  const { data } = useGetCustomerAppointmentQuery();
-  const chartData = React.useMemo(() => {
+export default function MyAreaChart() {
+  const { data, refetch } = useGetCustomerAppointmentQuery();
+  const isFocused = useRef(true);
+
+  useEffect(() => {
+    const handleFocus = () => {
+      isFocused.current = true;
+      refetch();
+    };
+
+    window.addEventListener("focus", handleFocus);
+
+    return () => {
+      window.removeEventListener("focus", handleFocus);
+    };
+  }, [refetch]);
+  
+  const chartData = useMemo(() => {
     if (data?.details) {
-      const serviceCounts = data?.details.reduce((acc, item) => {
+      const serviceCounts = data?.details?.reduce((acc, item) => {
         const key = item._id;
         if (!acc[key]) {
           acc[key] = {
@@ -35,38 +51,36 @@ export default function MyPieChart() {
     return [];
   }, [data]);
 
-  const COLORS = React.useMemo(() => {
+  const COLORS = useMemo(() => {
     return randomColor({ count: chartData?.length, luminosity: "bright" });
   }, [chartData]);
 
   return (
     <ResponsiveContainer height={400}>
       <h3 className="text-center text-lg">Appointment Customers</h3>
-      <PieChart>
-        <Pie
-          data={chartData}
-          dataKey="quantity"
-          nameKey="name"
-          cx="50%"
-          cy="50%"
-          outerRadius={150}
-          fill="#8884d8"
-          label
-        >
-          {chartData?.map((entry, index) => (
-            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS?.length]} />
-          ))}
-        </Pie>
+      <AreaChart data={chartData}>
+        <XAxis dataKey="name" />
+        <YAxis />
         <Tooltip />
         <Legend
           align="center"
           verticalAlign="bottom"
           layout="horizontal"
           formatter={(value, entry) =>
-            `${entry.payload.name} (${entry.payload.quantity})`
+            `${entry?.payload?.name}`
           }
         />
-      </PieChart>
+        {chartData?.map((entry, index) => (
+          <Area
+            key={`area-${index}`}
+            type="monotone"
+            dataKey="quantity"
+            name={entry.name}
+            stroke={COLORS[index % COLORS?.length]}
+            fill={COLORS[index % COLORS?.length]}
+          />
+        ))}
+      </AreaChart>
     </ResponsiveContainer>
   );
 }
