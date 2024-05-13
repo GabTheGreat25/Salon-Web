6a47424fa9f536853473cddfe9292e84fc19d87b
Jay200123 <kenken.ayonon@gmail.com>
2024-05-13 13:10:06 +0800

    Change receptionist terms

--- src/pages/TermsAndConditions/ReceptionistRegisterTermsCondition.jsx
+++ src/pages/TermsAndConditions/ReceptionistRegisterTermsCondition.jsx
@@ -15,7 +15,8 @@ export default function () {
           Terms And Conditions
         </h1>
         <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
-          By registering as a beautician, you agree to the following terms:
+          By registering as a receptionist at Lhanlee Salon, you agree to the
+          following terms:
         </h3>
         <br />
         <br />
@@ -26,15 +27,11 @@ export default function () {
         <br />
         <br />
         <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
-          During registration, you will select available dates, specifically
-          Mondays, for a face-to-face meeting with the salon owner.
-        </h3>
-        <br />
-        <br />
-        <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
-          It is your responsibility to be physically present at the salon for
-          the scheduled meeting on the selected Monday, and failure to attend
-          may result in the rejection of your hiring process.
+          To initiate the process, you must complete the registration on our
+          website, providing accurate and up-to-date information. After
+          Registration , It is your responsibility to be physically present at
+          the salon for the scheduled meeting on the selected day and failure to
+          attend may result in the rejection of your hiring process.
         </h3>
         <br />
         <br />
@@ -42,18 +39,7 @@ export default function () {
           Following the face-to-face meeting, the salon owner will review your
           application within 24 hours. If accepted, you will be notified via
           SMS, and you can proceed with the hiring process. In case of
-          rejection, your registration may be deleted by the administrator or
-          automatically removed after 7 days.
-        </h3>
-        <br />
-        <br />
-        <h3 className="font-light text-justify 2xl:text-4xl md:text-3xl">
-          The availability of dates for face-to-face meetings is not guaranteed
-          and may be subject to rescheduling or cancellation by the salon owner.
-          The salon reserves the right to terminate the registration and hiring
-          process at any stage if inaccurate information is provided or if there
-          is a violation of salon policies, with termination occurring without
-          notice.
+          rejection, your registration may be deleted by the administrator.
         </h3>
         <br />
         <br />
